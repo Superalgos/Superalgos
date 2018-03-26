2d4398574a518a8df0ca90023f909cfa94869855
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-03-26 16:09:06 +0200

    More logs improvements

--- Multi-Period-Daily/User.bot.js
+++ Multi-Period-Daily/User.bot.js
@@ -108,6 +108,7 @@ Read the candles and volumes from Bruce and produce a file for each day and for 
                     let reportKey;
 
                     reportKey = "AAMasters" + "-" + "AACharly" + "-" + "Poloniex-Historic-Trades" + "-" + "dataSet.V1";
+                    if (FULL_LOG === true) { logger.write("[INFO] start -> getContextVariables -> reportKey = " + reportKey); }
                     thisReport = dependencies.statusReports.get(reportKey).file;
 
                     if (thisReport.lastFile === undefined) {
@@ -119,6 +120,7 @@ Read the candles and volumes from Bruce and produce a file for each day and for 
                     contextVariables.firstTradeFile = new Date(thisReport.lastFile.year + "-" + thisReport.lastFile.month + "-" + thisReport.lastFile.days + " " + thisReport.lastFile.hours + ":" + thisReport.lastFile.minutes + GMT_SECONDS);
 
                     reportKey = "AAMasters" + "-" + "AABruce" + "-" + "One-Min-Daily-Candles-Volumes" + "-" + "dataSet.V1" + bot.processDatetime.getUTCFullYear() + "-" + utilities.pad(bot.processDatetime.getUTCMonth(),2);
+                    if (FULL_LOG === true) { logger.write("[INFO] start -> getContextVariables -> reportKey = " + reportKey); }
                     thisReport = dependencies.statusReports.get(reportKey).file;
 
                     if (thisReport.lastFile === undefined) {
@@ -130,6 +132,7 @@ Read the candles and volumes from Bruce and produce a file for each day and for 
                     contextVariables.maxCandleFile = new Date(thisReport.lastFile.year + "-" + thisReport.lastFile.month + "-" + thisReport.lastFile.days + " " + thisReport.lastFile.hours + ":" + thisReport.lastFile.minutes + GMT_SECONDS);
 
                     reportKey = "AAMasters" + "-" + "AAOlivia" + "-" + "Multi-Period-Daily" + "-" + "dataSet.V1";
+                    if (FULL_LOG === true) { logger.write("[INFO] start -> getContextVariables -> reportKey = " + reportKey); }
                     thisReport = dependencies.statusReports.get(reportKey).file;
 
                     if (thisReport.lastFile !== undefined) {

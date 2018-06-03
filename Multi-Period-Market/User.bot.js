8b2d7addc8648d1d755f754c63f79bfafba9acbe
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-06-03 23:01:34 +0200

    Initialize Logger

--- Multi-Period-Market/User.bot.js
+++ Multi-Period-Market/User.bot.js
@@ -46,6 +46,7 @@
         try {
 
             logger.fileName = MODULE_NAME;
+            logger.initialize();
 
             if (FULL_LOG === true) { logger.write("[INFO] initialize -> Entering function."); }
 

a67d383c88cac820479854d1bbd69305d90492f9
Matias Benitez <mati108@gmail.com>
2018-06-04 18:26:53 -0300

    Bot Update
    Including several changes.

--- Multi-Period-Market/User.bot.js
+++ Multi-Period-Market/User.bot.js
@@ -1,6 +1,4 @@
-﻿var LRCIndicator = require('./LRCIndicator');
-
-exports.newUserBot = function newUserBot(BOT, COMMONS, UTILITIES, DEBUG_MODULE, BLOB_STORAGE, FILE_STORAGE) {
+﻿exports.newUserBot = function newUserBot(BOT, COMMONS, UTILITIES, DEBUG_MODULE, BLOB_STORAGE) {
     const FULL_LOG = true;
     const LOG_FILE_CONTENT = false;
     const USE_PARTIAL_LAST_CANDLE = true; // When running live the last candle generated is a partial candle.
@@ -381,7 +379,7 @@ exports.newUserBot = function newUserBot(BOT, COMMONS, UTILITIES, DEBUG_MODULE, 
                                 if (FULL_LOG === true) { logger.write("[INFO] start -> saveChannel -> Entering function."); }
                                 let fileContent;
                                 let datePath = executionTime.getUTCFullYear() + "/" + utilities.pad(executionTime.getUTCMonth() + 1, 2) + "/" + utilities.pad(executionTime.getUTCDate(), 2);
-                                let filePath = bot.filePathRoot + "/Output/LRC-Channel/Multi-Period-Daily/" + folderName + "/" + datePath;
+                                let filePath = bot.filePathRoot + "/Output/LRC-Points/Multi-Period-Daily/" + folderName + "/" + datePath;
                                 let fileName = market.assetA + '_' + market.assetB + ".json"
 
                                 if (FULL_LOG === true) { logger.write("[INFO] start -> buildLRCChannels -> periodsLoop -> loopBody -> getCurrentContent -> fileName = " + fileName); }
@@ -578,3 +576,114 @@ exports.newUserBot = function newUserBot(BOT, COMMONS, UTILITIES, DEBUG_MODULE, 
         }
     }
 };
+
+/*
+ * Linear regression curve. SRC: Gekko
+ */
+
+//var log = require('../../core/log');
+
+var LRCIndicator = function (settings) {
+    this.input = 'price';
+    this.depth = settings;
+    this.result = false;
+    this.age = 0;
+    this.history = [];
+    this.x = [];
+    /*
+     * Do not use array(depth) as it might not be implemented
+     */
+    for (var i = 0; i < this.depth; i++) {
+        this.history.push(0.0);
+        this.x.push(i);
+    }
+
+    // log.debug("Created LRC indicator with h: ", this.depth);
+}
+
+LRCIndicator.prototype.update = function (price) {
+    // We need sufficient history to get the right result. 
+    if (this.result === false && this.age < this.depth) {
+
+        this.history[this.age] = price;
+        this.age++;
+        this.result = false;
+        // log.debug("Waiting for sufficient age: ", this.age, " out of ", this.depth);
+        return;
+    }
+
+    this.age++;
+    // shift history
+    for (var i = 0; i < (this.depth - 1); i++) {
+        this.history[i] = this.history[i + 1];
+    }
+    this.history[this.depth - 1] = price;
+
+    this.calculate(price);
+
+    // log.debug("Checking LRC: ", this.result.toFixed(8), "\tH: ", this.age);
+    return;
+}
+
+/*
+ * Least squares linear regression fitting.
+ */
+function linreg(values_x, values_y) {
+    var sum_x = 0;
+    var sum_y = 0;
+    var sum_xy = 0;
+    var sum_xx = 0;
+    var count = 0;
+
+    /*
+     * We'll use those variables for faster read/write access.
+     */
+    var x = 0;
+    var y = 0;
+    var values_length = values_x.length;
+
+    if (values_length != values_y.length) {
+        throw new Error('The parameters values_x and values_y need to have same size!');
+    }
+
+    /*
+     * Nothing to do.
+     */
+    if (values_length === 0) {
+        return [[], []];
+    }
+
+    /*
+     * Calculate the sum for each of the parts necessary.
+     */
+    for (var v = 0; v < values_length; v++) {
+        x = values_x[v];
+        y = values_y[v];
+        sum_x += x;
+        sum_y += y;
+        sum_xx += x * x;
+        sum_xy += x * y;
+        count++;
+    }
+
+    /*
+     * Calculate m and b for the formula: y = x * m + b
+     * 
+     */
+    var m = (count * sum_xy - sum_x * sum_y) / (count * sum_xx - sum_x * sum_x);
+    var b = (sum_y / count) - (m * sum_x) / count;
+
+    return [m, b];
+}
+
+
+/*
+ * Handle calculations
+ */
+LRCIndicator.prototype.calculate = function (price) {
+    // get the reg
+    var reg = linreg(this.x, this.history);
+
+    // y = a * x + b
+    this.result = ((this.depth - 1) * reg[0]) + reg[1];
+}

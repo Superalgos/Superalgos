75feb94afca6bfe617cb300bdef107b96f51226a
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-09-03 15:36:53 +0200

    Git ignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 
 // Components
 
 import UserList from './components/UserList';
 
+// Apollo Client Setup
+
+const client = new ApolloClient({
+  uri:'http://localhost:4000/graphql'
+})
+
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Advanced Algos - Users Module</h1>
-        <UserList></UserList>
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Advanced Algos - Users Module</h1>
+          <UserList></UserList>
+        </div>
+      </ApolloProvider>
     );
   }
 }

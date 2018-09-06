153dbf96631777d7c019b7b24142469298671bb1
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-09-06 07:47:56 +0200

    Profile and Browse splitted

--- client/src/components/Browse.js
+++ client/src/components/Browse.js
@@ -1,12 +1,10 @@
 import React from 'react'
+import UserList from './UserList';
 
 const Browse = () => {
   return (
     <div>
-      <div className="container">
-        <h4 className="center">Browse</h4>
-        <p>Here we will have the Browse functionality. we will allow end users to navigate through a directory like list of system users. We need to resolve pagination and be able to filter users by their role. </p>
-      </div>
+      <UserList/>
     </div>
   )
 }
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import UserList from './UserList';
 import AddUser from './AddUser';
 
 const Profile = () => {
   return (
-    <div id="main">
-      <UserList/>
+    <div>
       <AddUser/>
     </div>
   )

327bfcc038d23c46af98dd62bbccb96dfe39f058
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-09-06 07:55:01 +0200

    dummy html cleaned

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,8 +9,6 @@ import React, { Component } from 'react'
 import PortraitImage from '../img/portrait.jpg'
 import CityImage from '../img/city.jpg'
 import NatureImage from '../img/nature.jpg'
-import StreetImage from '../img/street.jpg'
-import StarsImage from '../img/stars.jpg'
 
 class Home extends Component {
 
@@ -48,54 +46,6 @@ class Home extends Component {
           </div>
         </div>
       </section>
-
-
-      <div className="parallax-container">
-        <div className="parallax">
-          <img src={StreetImage} alt="" className="responsive-img"></img>
-        </div>
-      </div>
-
-
-      <section className="section container scrollspy" id="services">
-        <div className="row">
-          <div className="col s12 l4">
-            <h2 className="indigo-text text-darken-4">What I do..</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at lacus congue, suscipit elit nec, tincidunt orci.</p>
-            <p>Mauris dolor augue, vulputate in pharetra ac, facilisis nec libero. Fusce condimentum gravida urna, vitae scelerisque erat ornare nec.</p>
-          </div>
-          <div className="col s12 l6 offset-l2">
-
-            <ul className="tabs">
-              <li className="tab col s6">
-                <a href="#photography" className="active indigo-text text-darken-4">Photography</a>
-              </li>
-              <li className="tab col s6">
-                <a href="#editing" className="indigo-text text-darken-4">Editing</a>
-              </li>
-            </ul>
-            <div id="photography" className="col s12">
-                <p className="flow-text indigo-text text-darken-4">This</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at lacus congue, suscipit elit nec, tincidunt orci.</p>
-                <p>Mauris dolor augue, vulputate in pharetra ac, facilisis nec libero. Fusce condimentum gravida urna, vitae scelerisque erat ornare nec.</p>
-            </div>
-            <div id="editing" className="col s12">
-                <p className="flow-text indigo-text text-darken-4">That</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at lacus congue, suscipit elit nec, tincidunt orci.</p>
-                <p>Mauris dolor augue, vulputate in pharetra ac, facilisis nec libero. Fusce condimentum gravida urna, vitae scelerisque erat ornare nec.</p>
-            </div>
-            </div>
-          </div>
-      </section>
-
-
-      <div className="parallax-container">
-        <div className="parallax">
-          <img src={StarsImage} alt="" className="responsive-img"></img>
-        </div>
-      </div>
-
-
 
 
       </div>
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,45 +1,15 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import Pokeball from '../pokeball.png'
 
 class Search extends Component {
   state = {
     posts: []
   }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts/')
-      .then(res => {
-        this.setState({
-          posts: res.data.slice(0,10)
-        });
-      })
-  }
-  render(){
-    const { posts } = this.state
-    const postList = posts.length ? (
-      posts.map(post => {
-        return (
-          <div className="post card" key={post.id}>
-            <img src={Pokeball} alt="A Pokeball" />
-            <div className="card-content">
-              <Link to={'/' + post.id}>
-                <span className="card-title red-text">{post.title}</span>
-              </Link>
-              <p>{post.body}</p>
-            </div>
-          </div>
-        )
-      })
-    ) : (
-      <div className="center">No posts to show</div>
-    );
 
+  render(){
     return (
       <div>
-        <div className="container home">
-          <h4 className="center">Search</h4>
-          {postList}
+        <div>
+
         </div>
       </div>
     )

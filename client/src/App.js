5858ec16249104cfd2a9d0c95d910b7b7ef30983
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-09-09 11:09:28 +0200

    Refactoring file name to Auth

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ import Post from './components/Post';
 import Footer from './components/Footer';
 import Callback from './components/Callback';
 
-import Auth from './auth/index';
+import Auth from './auth/Auth';
 
 // Apollo Client Setup
 const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql', changeOrigin: true })

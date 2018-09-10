dcfaae63a61e1e77db1a13245feef82f3c5589da
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-09-10 11:31:43 +0200

    More code cleaning

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,8 +7,6 @@ import React, { Component } from 'react'
 // Images
 
 import PortraitImage from '../img/portrait.jpg'
-import CityImage from '../img/city.jpg'
-import NatureImage from '../img/nature.jpg'
 
 class Home extends Component {
 
@@ -24,24 +22,6 @@ class Home extends Component {
           </div>
           <div className="col s12 l6 offset-l1">
             <h2 className="indigo-text text-darken-4">Title 1</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at lacus congue, suscipit elit nec, tincidunt orci.</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col s12 l4 offset-l1 push-l7">
-              <img src={CityImage} alt="" className="responsive-img materialboxed"></img>
-          </div>
-          <div className="col s12 l6 offset-l1 pull-l5 right-align">
-            <h2 className="indigo-text text-darken-4">Title 2</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at lacus congue, suscipit elit nec, tincidunt orci.</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col s12 l4">
-              <img src={NatureImage} alt="" className="responsive-img materialboxed"></img>
-          </div>
-          <div className="col s12 l6 offset-l1">
-            <h2 className="indigo-text text-darken-4">Title 3</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at lacus congue, suscipit elit nec, tincidunt orci.</p>
           </div>
         </div>

56123621505b9e64a9e654babff286dbe8f68ae5
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-09-12 11:49:33 +0200

    Footer Cleaned

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import AALogo from '../img/aa-logo-dark-8.png'
 
 const Footer = () => {
   return (
     <footer>
-      <div className="container">
-        <div className="row">
-          <div className="col l6 s12">
-            <h5 className="white-text">Footer Content</h5>
-            <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+
+          <div>
+            <h5>Footer Content</h5>
+            <p>You can use rows and columns here to organize your footer content.</p>
           </div>
-          <div className="col l4 offset-l2 s12">
-            <h5 className="white-text">Links</h5>
-            <ul className="right hide-on-med-and-down">
+          <div>
+            <h5>Links</h5>
+            <ul>
               <li><NavLink exact to="/">Home</NavLink></li>
               <li><NavLink to='/browse'>Browse</NavLink></li>
               <li><NavLink to='/search'>Search</NavLink></li>
@@ -20,26 +20,14 @@ const Footer = () => {
               <li><NavLink to='/about'>About</NavLink></li>
               <li><a href="http://modules.advancedalgos.net">Modules</a></li>
 
-              <li><a href="" className="tooltipped btn-floating btn-small indigo darken-4" data-position="bottom" data-tooltip="You Tube">
-                <i className="fab fa-youtube"></i>
-              </a></li>
-              <li><a href="" className="tooltipped btn-floating btn-small indigo darken-4" data-position="bottom" data-tooltip="Facebook">
-                <i className="fab fa-facebook"></i>
-              </a></li>
-              <li><a href="" className="tooltipped btn-floating btn-small indigo darken-4" data-position="bottom" data-tooltip="Twitter">
-                <i className="fab fa-twitter"></i>
-              </a></li>
-
             </ul>
           </div>
+
+
+        <div>
+        © 2018 Copyright <a href="www.advancedalgos.net">Advanced Algos Ltd.</a>
         </div>
-      </div>
-      <div className="footer-copyright">
-        <div className="container">
-        © 2018 Copyright
-        <a className="grey-text text-lighten-4 right" href="www.advancedalgos.net">Advanced Algos Ltd.</a>
-        </div>
-      </div>
+       
     </footer>
   )
 }

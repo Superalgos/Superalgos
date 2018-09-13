8778db4f92a98d00b973f2b6dfaa28ab2307e96c
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-09-13 17:04:44 +0200

    Dummy Chart at home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,36 +1,65 @@
 import React, { Component } from 'react'
+import { compose } from 'recompose'
+import { withStyles } from '@material-ui/core/styles'
+import {LineChart} from 'react-easy-chart'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
 
-// Components
-
-//import ChartUsers from './home/ChartUsers'
-
-// Images
-
-import PortraitImage from '../img/portrait.jpg'
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    padding: 100
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: 120,
+    maxHeight: 24
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+})
 
 class Home extends Component {
 
-  render(){
-
+  render () {
+    const { classes } = this.props
     return (
-      <div>
+      <Paper className={classes.root}>
+        <Grid className={classes.button} container justify='center' spacing={24}>
+          <Grid item>
+            <LineChart
+              xType={'time'}
+              axes
+              interpolate={'cardinal'}
+              width={750}
+              height={250}
+              data={[
+                [
+              { x: '1-Jan-15', y: 20 },
+              { x: '1-Feb-15', y: 10 },
+              { x: '1-Mar-15', y: 33 },
+              { x: '1-Apr-15', y: 45 },
+              { x: '1-May-15', y: 15 }
+                ], [
+              { x: '1-Jan-15', y: 10 },
+              { x: '1-Feb-15', y: 15 },
+              { x: '1-Mar-15', y: 13 },
+              { x: '1-Apr-15', y: 15 },
+              { x: '1-May-15', y: 10 }
+                ]
+              ]}
+        />
+          </Grid>
+        </Grid>
 
-      <section className="container section scrollspy" id="photos">
-        <div className="row">
-          <div className="col s12 l4">
-              <img src={PortraitImage} alt="" className="responsive-img materialboxed"></img>
-          </div>
-          <div className="col s12 l6 offset-l1">
-            <h2 className="indigo-text text-darken-4">Title 1</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at lacus congue, suscipit elit nec, tincidunt orci.</p>
-          </div>
-        </div>
-      </section>
+      </Paper>
 
-
-      </div>
     )
   }
 }
 
-export default Home
+export default compose(
+  withStyles(styles)
+)(Home)

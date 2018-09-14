5b90eb8923b436133fbfaf6d28a8d90ebe40a40c
Barry Low <barrylow@gmail.com>
2018-09-14 07:35:25 -0700

    Client: Team Details subpage

---
+++ client/src/views/teams/components/TeamsDetails.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import { withStyles } from '@material-ui/core/styles'
+
+import { Link } from 'react-router-dom'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 6,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+      width: 900,
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3
+    }
+  },
+  heroContent: {
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`
+  }
+})
+
+const TeamsDetails = ({ classes, team }) => (
+  <React.Fragment>
+    <CssBaseline />
+    <main className={classes.layout}>
+      <Paper className={classes.paper}>
+        <div className={classes.heroContent}>
+          <Link to='/teams'>&larr; Back to all teams</Link>
+          <Typography
+            variant='display3'
+            align='center'
+            color='textPrimary'
+            gutterBottom
+          >
+            {team.name}
+          </Typography>
+          <Typography variant='subheading' color='textSecondary'>
+            {team.createdAt}
+          </Typography>
+          <Typography variant='subheading' paragraph gutterBottom>
+            Members: {team.members.length}
+          </Typography>
+          <Typography variant='subheading' color='primary'>
+            Team Admin:
+            {
+              team.members.map(member => {
+                if (member.role === 'OWNER' || member.role === 'ADMIN') {
+                  return member.member.alias
+                }
+              })
+            }
+          </Typography>
+        </div>
+      </Paper>
+    </main>
+  </React.Fragment>
+)
+TeamsDetails.propTypes = {
+  classes: PropTypes.object.isRequired,
+  team: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(TeamsDetails)
--- client/src/views/teams/components/TeamsList.js
+++ client/src/views/teams/components/TeamsList.js
@@ -4,58 +4,125 @@ import { Query } from 'react-apollo'
 
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core/styles'
 
 import GET_ALL_TEAMS_QUERY from '../../../graphql/teams/GetAllTeamsQuery'
 
-import TeamsItem from './TeamsItem'
+import { isEmpty } from '../../../utils/js-helpers'
 
-export const TeamsList = ({ classes }) => (
-  <Grid item md={12}>
-    <Query query={GET_ALL_TEAMS_QUERY}>
-      {({ loading, error, data }) => {
-        console.log('GET_TEAMS_BY_OWNER: ', loading, error, data)
-        let errors
-        let queryLoader
-        if (error) {
-          errors = error.graphQLErrors.map(({ message }, i) => {
+import TeamsItem from './TeamsItem'
+import TeamsDetails from './TeamsDetails'
+
+const styles = theme => ({
+  heroContent: {
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  cardMedia: {
+    paddingTop: '56.25%' // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  buttonRight: {
+    justifyContent: 'flex-end'
+  }
+})
+
+export const TeamsList = ({ classes, match }) => (
+  <Query query={GET_ALL_TEAMS_QUERY} fetchPolicy='cache-and-network' >
+    {({ loading, error, data }) => {
+      console.log('GET_TEAMS_BY_OWNER: ', loading, error, data, match)
+      let slug = null
+      if (!isEmpty(match.params) && match.params.slug) slug = match.params.slug
+
+      let errors
+      let queryLoader
+      if (error) {
+        errors = error.graphQLErrors.map(({ message }, i) => {
+          return (
+            <Typography key={i} variant='caption'>
+              {message}
+            </Typography>
+          )
+        })
+      }
+      if (!loading && !error) {
+        if (data.teams.edges.length > 0) {
+          if (slug !== null) {
             return (
-              <Typography key={i} variant='caption'>
-                {message}
-              </Typography>
-            )
-          })
-        }
-        if (!loading && !error) {
-          if (data.teams.edges.length > 0) {
-            return (
-              <Grid container spacing={24}>
+              <Grid container spacing={40}>
                 {!loading &&
-                  data.teams.edges.map(team => (
-                    <TeamsItem key={team.node.id} team={team.node} />
-                  ))}
+                  data.teams.edges.map(team => {
+                    if (team.node.slug === slug) {
+                      return (
+                        <TeamsDetails
+                          key={team.node.id}
+                          team={team.node}
+                        />
+                      )
+                    }
+                  })}
                 {queryLoader}
                 {errors}
               </Grid>
             )
+          } else {
+            return (
+              <React.Fragment>
+                <div className={classes.heroContent}>
+                  <Typography
+                    variant='display3'
+                    align='center'
+                    color='textPrimary'
+                    gutterBottom
+                  >
+                    Teams
+                  </Typography>
+                </div>
+                <Grid container spacing={40}>
+                  {!loading &&
+                    data.teams.edges.map(team => (
+                      <TeamsItem
+                        key={team.node.id}
+                        team={team.node}
+                        classes={classes}
+                      />
+                    ))}
+                  {queryLoader}
+                  {errors}
+                </Grid>
+              </React.Fragment>
+            )
           }
-        } else {
-          return (
-            <Grid container spacing={24}>
-              <Typography variant='subheading' gutterBottom>
-                Loading...
-              </Typography>
-              {queryLoader}
-              {errors}
-            </Grid>
-          )
         }
-      }}
-    </Query>
-  </Grid>
+      } else {
+        return (
+          <Grid container spacing={40}>
+            <Typography variant='subheading' gutterBottom>
+              Loading...
+            </Typography>
+            {queryLoader}
+            {errors}
+          </Grid>
+        )
+      }
+    }}
+  </Query>
 )
 
 TeamsList.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  match: PropTypes.object
 }
 
-export default TeamsList
+export default withStyles(styles)(TeamsList)

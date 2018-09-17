54b735f9d5c52791aebbc88daca9c6c6e1b84ec0
Barry Low <barrylow@gmail.com>
2018-09-17 12:39:45 -0700

    Client: simplify to pass user info via props

---
+++ client/src/views/dashboard/FinancialBeings.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+
+import { MessageCard } from '../common/'
+
+const styles = theme => ({
+  tableContainer: {
+    height: 320
+  }
+})
+
+const FinancialBeings = ({ classes }) => (
+  <div>
+    <Typography variant='display1' gutterBottom>
+      Financial Beings
+    </Typography>
+    <div className={classes.tableContainer}>
+      <MessageCard message='Coming soon. View your financial beings by team.' />
+    </div>
+  </div>
+)
+
+FinancialBeings.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(FinancialBeings)

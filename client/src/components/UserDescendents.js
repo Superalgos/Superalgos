81a93b92a6ef13dc83d27af01380c165b0a89ce1
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-09-27 11:56:57 +0200

    Your Referrer Form there

---
+++ client/src/components/UserDescendents.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react'
+import {graphql, compose} from 'react-apollo'
+import {getUsersBySearchFields} from '../queries/queries'
+
+// Materia UI
+
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+
+import TextField from '@material-ui/core/TextField'
+
+// components
+import UserProfile from './UserProfile'
+
+// Images
+import PortraitImage from '../img/portrait.jpg'
+
+const styles = theme => ({
+  card: {
+    maxWidth: 150,
+    minWidth: 150,
+    paddingTop: '30',
+    marginBottom: 40
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9,
+    marginTop: '30',
+    minWidth: 300
+  },
+  typography: {
+    maxWidth: 345,
+    minWidth: 300,
+    paddingTop: '30'
+  },
+  appBar: {
+    position: 'relative'
+  },
+  flex: {
+    flex: 1
+  },
+  grid: {
+    paddingTop: '30',
+    marginTop: 30
+  },
+  inputField: {
+    width: '80%',
+    marginLeft: '10%',
+    marginTop: 25
+  },
+  formTypography: {
+    width: '80%',
+    marginLeft: '10%',
+    marginTop: 40
+  },
+  button: {
+    margin: theme.spacing.unit,
+    marginTop: theme.spacing.unit * 3
+  }
+})
+
+class UserDescendents extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      alias: '',
+      firstName: '',
+      middleName: '',
+      lastName: ''
+    }
+  }
+
+  submitForm (e) {
+    e.preventDefault()
+
+    this.props.getUsersBySearchFields.variables.alias = this.state.alias
+    this.props.getUsersBySearchFields.variables.firstName = this.state.firstName
+    this.props.getUsersBySearchFields.variables.middleName = this.state.middleName
+    this.props.getUsersBySearchFields.variables.lastName = this.state.lastName
+
+    this.props.getUsersBySearchFields.refetch({})
+  }
+
+  displayUsers () {
+    let data = this.props.getUsersBySearchFields
+    const { classes } = this.props
+
+    if (data.loading) {
+      return (<div> Loading Users... </div>)
+    } else {
+      if (data.usersSearch === undefined) {
+        return (<div> No Users to Display </div>)
+      } else {
+        return data.usersSearch.map(user => {
+          return (
+
+            <Grid key={user.id} item>
+              <Card className={classes.card} onClick={(e) => {
+                this.setState({ selected: user.id})
+              }
+            }>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={PortraitImage}
+                    title='User Profile'
+
+                />
+                  <CardContent>
+                    <Typography gutterBottom variant='headline' component='h2'>
+                      {user.alias}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Grid container justify='center' spacing={8}>
+                    <Grid item>
+                      {this.selectButton(user)}
+                    </Grid>
+                  </Grid>
+                </CardActions>
+              </Card>
+            </Grid>
+
+          )
+        })
+      }
+    }
+  }
+
+  render () {
+    const { classes } = this.props
+    return (
+      <div>
+        <form onSubmit={this.submitForm.bind(this)}>
+
+          <Typography className={classes.formTypography} variant='body1' gutterBottom align='left'>
+           These are your decendents within the project. Your children referred you as the one who brought them to the project, while
+           your grandchildren referred your children and so on.
+          </Typography>
+
+          <Grid container justify='center' >
+            <Grid item>
+              <Button variant='contained' color='secondary' className={classes.button} onClick={this.submitForm.bind(this)}>Search</Button>
+            </Grid>
+          </Grid>
+        </form>
+
+        <Grid container className={classes.grid} justify='center' spacing={24}>
+          {this.displayUsers()}
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default compose(
+  graphql(getUsersBySearchFields, {name: 'getUsersBySearchFields'}),
+  withStyles(styles)
+)(UserDescendents) // This technique binds more than one query to a single component.
--- client/src/components/UserReferrals.js
+++ client/src/components/UserReferrals.js
@@ -28,11 +28,17 @@ const styles = theme => ({
   typography: {
     width: '80%',
     marginLeft: '10%',
-    marginTop: 40
+    marginTop: 40,
+    marginBottom: 40
   },
   button: {
     margin: theme.spacing.unit,
     marginTop: theme.spacing.unit * 3
+  },
+  inputField: {
+    width: '80%',
+    marginLeft: '10%',
+    marginTop: 25
   }
 })
 
@@ -74,6 +80,74 @@ class UserReferrals extends Component {
     localStorage.setItem('loggedInUser', JSON.stringify(user))
   }
 
+  renderMode () {
+    const { classes } = this.props
+
+    if (this.state.referrerId !== '') {
+      return (
+        <div>
+          <Typography className={classes.typography} variant='body1' gutterBottom align='left'>
+          This is the user who introduced you to the project.
+          </Typography>
+
+          <TextField
+            id='alias'
+            type='text'
+            value={this.state.alias}
+            label='Alias'
+            className={classes.inputField}
+            disabled
+                     />
+
+          <TextField
+            id='firstName'
+            type='text'
+            value={this.state.firstName}
+            label='First Name'
+            className={classes.inputField}
+            disabled
+                     />
+
+          <TextField
+            id='middleName'
+            type='text'
+            value={this.state.middleName}
+            label='Middle Name'
+            className={classes.inputField}
+            disabled
+                     />
+
+          <TextField
+            id='lastName'
+            type='text'
+            value={this.state.lastName}
+            label='Last Name'
+            className={classes.inputField}
+            disabled
+                     />
+
+          <Typography className={classes.typography} variant='body1' gutterBottom align='left'>
+           This information can not be changed.
+           </Typography>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Typography className={classes.typography} variant='body1' gutterBottom align='left'>
+          We know that a group often becomes more intelligent when the number of people in the group increases.
+          To incentivize members of the community to invite more people to join we have developed this referral
+          program in which you can specify who referred you to the project, and see the members who pointed at
+          you as their referrer. Later this information will be available at your user profile and add to your
+          overall reputation within the project.
+          </Typography>
+
+          <UserSearch selectButton onSelect={this.userSelected.bind(this)} selectText="After that press the 'Select' button on the desired user to set it as your referrer. Bear in mind that this action can not be undone." />
+        </div>
+      )
+    }
+  }
+
   componentWillMount ()    	{
     	    if (this.defaultValuesSet === false)    	    {
           let userData = localStorage.getItem('loggedInUser')
@@ -97,21 +171,11 @@ class UserReferrals extends Component {
     return (
       <Paper className={classes.root}>
         <Typography className={classes.typography} variant='headline' gutterBottom>
-              Referral Program
-        </Typography>
+            Your Referrer
+      </Typography>
 
-        <Typography className={classes.typography} variant='body1' gutterBottom align='left'>
-        We know that a group often becomes more intelligent when the number of people in the group increases.
-        To incentivize members of the community to invite more people to join we have developed this referral
-        program in which you can specify who referred you to the project, and see the members who pointed at
-        you as their referrer. Later this information will be available at your user profile and add to your
-        overall reputation within the project.
-        </Typography>
-
-        <UserSearch selectButton onSelect={this.userSelected.bind(this)} selectText="After that press the 'Select' button on the desired user to set it as your referrer. Bear in mind that this action can not be undone." />
-
+        {this.renderMode()}
       </Paper>
-
     )
   }
 }

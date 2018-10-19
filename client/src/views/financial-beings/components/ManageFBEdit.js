1d95706b04123564673930b97ac3db6828872f21
Barry Low <barrylow@gmail.com>
2018-10-19 11:20:34 -0700

    Update default team and FB images

--- client/src/views/financial-beings/components/ManageFBEdit.js
+++ client/src/views/financial-beings/components/ManageFBEdit.js
@@ -68,7 +68,7 @@ export class ManageFBEdit extends Component {
           const containerName = slug
 
           let avatar = null
-          if (this.state.avatar === null && fb.avatar === undefined) avatar = 'https://algobotcommstorage.blob.core.windows.net/aateammodule/aa-avatar-default.png'
+          if (this.state.avatar === null && fb.avatar === undefined) avatar = 'https://aadevelop.blob.core.windows.net/module-teams/module-default/aa-avatar-default.png'
           if (fb.avatar !== undefined && fb.avatar !== null) avatar = fb.avatar
           if (this.state.avatar !== null) avatar = this.state.avatar
 

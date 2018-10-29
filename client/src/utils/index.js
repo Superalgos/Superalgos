347848154aabec61e6932a9f7f64542707899089
Nikola Bjelo <nikolabjelo@gmail.com>
2018-10-29 12:08:16 +0100

    Better date format (probably going to make it settable)

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,6 @@
 export const toLocalTime = (epoch) => {
   let localDate = new Date(0)
   localDate.setUTCSeconds(epoch)
-  return localDate.toString()
+  var options = { year: 'numeric', month: 'long', day: '2-digit' }
+  return localDate.toLocaleDateString(options) + ' ' + localDate.toLocaleTimeString('en-US')
 }

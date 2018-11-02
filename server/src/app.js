ae283718714cce092c796f075b4b38026478baf0
Nikola Bjelo <nikola@bjelo.ch>
2018-11-02 16:01:25 +0100

    Switching to apollo server

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
-import cors from 'cors';
-import graphqlHTTP from 'express-graphql';
-import expressPlayground from 'graphql-playground-middleware-express';
+import { ApolloServer } from 'apollo-server-express';
 import * as Sentry from '@sentry/node';
 import schema from './schema';
 
@@ -12,27 +10,27 @@ const app = express();
 Sentry.init({ dsn: process.env.SENTRY_DNS });
 app.use(Sentry.Handlers.requestHandler());
 
-// authorising CORS
-app.use(cors());
 
 // Starting the graphql server
-app.use('/graphql', graphqlHTTP(req => ({
+const server = new ApolloServer({
   schema,
-  context: {
-    userId: req.headers.userid,
-    authorization: req.headers.authorization,
+  playground: {
+    settings: {
+      'editor.theme': 'dark',
+      'editor.cursorShape': 'line',
+    },
   },
-  formatError(err) {
-    return {
-      message: err.message,
-      code: err.originalError && err.originalError.code,
-      path: err.path,
-    };
-  },
-})));
+  formatError: error => ({
+    code: error.extensions.exception.code,
+    message: error.message,
+    path: error.path,
+  }),
+});
 
-// Starting the graphql server
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+server.applyMiddleware({
+  app,
+  cors: true,
+});
 
 // Sentry error handling middleware if anything pass through
 app.use(Sentry.Handlers.errorHandler());

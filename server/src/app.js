fa02ee759341649a612220332ab7d787b60a6771
Nikola Bjelo <nikola@bjelo.ch>
2018-11-02 20:00:40 +0100

    Adding a preshared key verification middleware

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,21 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import * as Sentry from '@sentry/node';
 import schema from './schema';
+import wrongPreshared from './errors/notAllowed.json';
 
 const app = express();
 
 // Initialising sentry
 Sentry.init({ dsn: process.env.SENTRY_DNS });
 app.use(Sentry.Handlers.requestHandler());
+
+app.post('/graphql', (req, res, next) => {
+  if (req.headers.preshared === process.env.PRESHARED_GATEWAY_KEY) {
+    next();
+  } else {
+    res.send(wrongPreshared);
+  }
+});
 
 
 // Starting the graphql server

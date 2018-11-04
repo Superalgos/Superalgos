91b1a9959818379d104bd9daa966c746fb3360dd
Nikola Bjelo <nikola@bjelo.ch>
2018-11-04 16:37:05 +0100

    Stop using soon to be deprecated function

--- server/src/db.js
+++ server/src/db.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 
 mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_DOMAIN}`, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;

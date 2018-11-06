461248f5e46a3e37712b5519a252a8abd90fadcd
Nikola Bjelo <nikola@bjelo.ch>
2018-11-06 14:07:23 +0100

    First draft of the first page of the form

--- client/src/Components/Event/Create/index.js
+++ client/src/Components/Event/Create/index.js
@@ -3,8 +3,18 @@ import { Mutation } from 'react-apollo';
 import { DateTime } from 'luxon';
 
 import {
-  Button,
   TextField,
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+  MenuItem,
+  FormGroup,
+  Select,
+  FormHelperText,
+  Paper,
+  Grid,
+  Typography,
 } from '@material-ui/core';
 import { ChevronLeft, ChevronRight } from '@material-ui/icons';
 import { DateTimePicker } from 'material-ui-pickers';
@@ -28,6 +38,7 @@ class Create extends React.Component {
     const {
       name, description, startDatetime, finishDatetime,
     } = this.state;
+    const { classes } = this.props;
     return (
       <Mutation mutation={hostedEventsCalls.EVENTS_CREATEEVENT}
         update={(store, { data }) => {
@@ -36,7 +47,11 @@ class Create extends React.Component {
         }
       >
         {hostEvent => (
-          <div className='container'>
+
+          <Paper className={classes.root}>
+            <Typography className={classes.typography} variant='headline' gutterBottom>
+              Create a new event
+            </Typography>
             <form
               onSubmit={(e) => {
                 e.preventDefault();
@@ -52,47 +67,110 @@ class Create extends React.Component {
                 });
               }}
             >
+              <Typography className={classes.typography} variant='body1' gutterBottom align='left'>
+                These are the basic information you need to input in order to create an event.
+                There might be some heavy changes in the near future as the application is still at a very early stage.
+              </Typography>
+
+              <TextField
+                label='Public id'
+                value='Your public ID will be generated automatically'
+                className={classes.inputField}
+                disabled
+              />
+
               <TextField
                 label='Name'
                 value={name}
-                onChange={e => this.setState({ name: e.target.value })}
-                fullWidth
+                className={classes.inputField}
               />
               <TextField
                 multiline
                 label='Description'
                 value={description}
-                onChange={e => this.setState({ description: e.target.value })}
-                fullWidth
+                className={classes.inputField}
               />
-              <DateTimePicker
-                autoOk
-                disablePast
-                format="DD' at 'HH:mm"
-                ampm={false}
-                showTabs={false}
-                leftArrowIcon={<ChevronLeft />}
-                rightArrowIcon={<ChevronRight />}
-                value={startDatetime}
-                onChange={date => this.setState({ startDatetime: date })}
-                helperText='Competition start date'
-              />
-              <DateTimePicker
-                autoOk
-                disablePast
-                format="DD' at 'HH:mm"
-                ampm={false}
-                showTabs={false}
-                leftArrowIcon={<ChevronLeft />}
-                rightArrowIcon={<ChevronRight />}
-                value={finishDatetime}
-                onChange={date => this.setState({ finishDatetime: date })}
-                helperText='Competition finishing date'
-              />
-              <Button type='submit' variant='contained' color='secondary'>Create New Project</Button>
+
+              <Typography className={classes.typography} variant='body1' gutterBottom align='left'>
+                The dates are purely indicative, as it is important for you to make sure everything is
+                ready before manually triggering the beginning and end of the event.
+                If you are confident use the scheduler to start it for you(incoming).
+              </Typography>
+
+              <FormGroup row className={classes.inputField}>
+                <Grid container justify='space-around' >
+                  <Grid item>
+                    <DateTimePicker
+                      autoOk
+                      disablePast
+                      format="DD' at 'HH:mm"
+                      ampm={false}
+                      showTabs={false}
+                      leftArrowIcon={<ChevronLeft />}
+                      rightArrowIcon={<ChevronRight />}
+                      value={startDatetime}
+                      helperText='Competition start date'
+                    />
+                  </Grid>
+                  <Grid item>
+                    <DateTimePicker
+                      autoOk
+                      disablePast
+                      format="DD' at 'HH:mm"
+                      ampm={false}
+                      showTabs={false}
+                      leftArrowIcon={<ChevronLeft />}
+                      rightArrowIcon={<ChevronRight />}
+                      value={finishDatetime}
+                      helperText='Competition finishing date'
+                    />
+                  </Grid>
+                </Grid>
+              </FormGroup>
+
+              <FormControl className={classes.inputField}>
+                <InputLabel shrink htmlFor='select'>
+                  Select the formula
+                </InputLabel>
+                <Select
+                  id='select'
+                  value='1'
+                  input={<Input name='Role' id='role-label-placeholder' />}
+                  displayEmpty
+                  name='select'
+                  className={classes.selectEmpty}
+                >
+                  <MenuItem key='1' value='1'>moi</MenuItem>
+                  <MenuItem key='2' value='2'>toi</MenuItem>
+                  <MenuItem key='3' value='3'>nous</MenuItem>
+                </Select>
+                <FormHelperText> For new formula, select {'"'}create new{'"'} </FormHelperText>
+              </FormControl>
+              <FormControl className={classes.inputField}>
+                <InputLabel shrink htmlFor='select'>
+                  Select the Plotter
+                </InputLabel>
+                <Select
+                  id='select'
+                  value='1'
+                  input={<Input name='Role' id='role-label-placeholder' />}
+                  displayEmpty
+                  name='select'
+                  className={classes.selectEmpty}
+                >
+                  <MenuItem key='1' value='1'>moi</MenuItem>
+                  <MenuItem key='2' value='2'>toi</MenuItem>
+                  <MenuItem key='3' value='3'>nous</MenuItem>
+                </Select>
+                <FormHelperText> For new plotter, select {'"'}create new{'"'} </FormHelperText>
+              </FormControl>
+              <Grid container justify='center' >
+                <Grid item>
+                  <Button type='submit' variant='contained' color='secondary'>Create New Project</Button>
+                </Grid>
+              </Grid>
             </form>
-            <div className='form-close' onClick={() => this.clearTempStates()} />
-          </div>
+          </Paper>
         )}
       </Mutation>
     );
--- client/src/Components/Event/Create/styles.js
+++ client/src/Components/Event/Create/styles.js
@@ -1,4 +1,39 @@
-const styles = () => ({
+const styles = theme => ({
+  root: {
+    width: '50%',
+    flexGrow: 1,
+    padding: 10,
+    marginLeft: '25%',
+    marginTop: '2%',
+  },
+  inputField: {
+    width: '80%',
+    marginLeft: '10%',
+    marginTop: 25,
+  },
+  typography: {
+    width: '80%',
+    marginLeft: '10%',
+    marginTop: 40,
+  },
+  checkbox: {
+    width: 150,
+    marginLeft: '0%',
+    marginTop: 20,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2,
+  },
+  button: {
+    margin: theme.spacing.unit,
+    marginTop: theme.spacing.unit * 3,
+  },
+  img: {
+    display: 'block',
+    marginTop: 20,
+    maxWidth: 120,
+    maxHeight: 24,
+  },
 });
 
 export default styles;

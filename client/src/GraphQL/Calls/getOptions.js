6d83c37bf19db714a54a13e827326ef658bde2b8
Nikola Bjelo <nikola@bjelo.ch>
2018-11-06 16:40:23 +0100

    creating migrations and setting up fragments

--- client/src/GraphQL/Calls/getOptions.js
+++ client/src/GraphQL/Calls/getOptions.js
@@ -1,31 +1,45 @@
 import gql from 'graphql-tag';
+import { formulaInfo, plotterInfo } from '../Fragments';
 
 const EVENTS_FORMULAS = gql`
   query Events_Formulas{
     events_Formulas {
-      id
-      ownerId
-      isTemplate
-      name
+      ...FormulaInfo
     }
   }
+  ${formulaInfo}
 `;
 
 const EVENTS_PLOTTERS = gql`
   query Events_Plotters{
     events_Plotters {
-      id
-      ownerId
-      isTemplate
-      name
-      host
-      repo
-      moduleName
+      ...PlotterInfo
     }
   }
+  ${plotterInfo}
+`;
+
+const EVENTS_CREATEFORMULA = gql`
+  mutation Events_CreateEvent($event:events_EventInput!) {
+    events_CreateEvent(event: $event) {
+      ...FormulaInfo
+    }
+  }
+  ${formulaInfo}
+`;
+
+const EVENTS_CREATEPLOTTER = gql`
+  mutation Events_CreatePlotter($plotter:events_PlotterInput!) {
+    events_CreatePlotter(plotter: $plotter) {
+      ...PlotterInfo
+    }
+  }
+  ${plotterInfo}
 `;
 
 export default {
   EVENTS_PLOTTERS,
   EVENTS_FORMULAS,
+  EVENTS_CREATEFORMULA,
+  EVENTS_CREATEPLOTTER,
 };
--- client/src/GraphQL/Fragments/index.js
+++ client/src/GraphQL/Fragments/index.js
@@ -2,8 +2,14 @@ import {
   eventMinimalInfo,
   eventFullInfo,
 } from './event';
+import {
+  formulaInfo,
+  plotterInfo,
+} from './options';
 
 export {
   eventMinimalInfo,
   eventFullInfo,
+  formulaInfo,
+  plotterInfo,
 };

48b592914b635373c0e4e7f50ac2ba47e33a2416
Barry Low <barrylow@gmail.com>
2018-11-07 14:28:53 -0800

    Fix teamBySlug query

--- api/src/graphql/resolvers/Query/index.js
+++ api/src/graphql/resolvers/Query/index.js
@@ -1,7 +1,7 @@
 import { logger } from '../../../logger'
 
 import { member, owner } from './Member'
-import { teams, teamById, teamByName, teamWithRole, teamsByOwner, teamsByRole } from './Team'
+import { teams, teamById, teamByName, teamBySlug, teamWithRole, teamsByOwner, teamsByRole } from './Team'
 import{ fbByTeamMember } from './TeamMember'
 
 export const resolvers = {
@@ -10,6 +10,7 @@ export const resolvers = {
     teams: (parent, arg, ctx, info) => teams(parent, arg, ctx, info),
     teamById: (parent, arg, ctx, info) => teamById(parent, arg, ctx, info),
     teamByName: (parent, arg, ctx, info) => teamByName(parent, arg, ctx, info),
+    teamBySlug: (parent, arg, ctx, info) => teamBySlug(parent, arg, ctx, info),
     teamWithRole: (parent, arg, ctx, info) => teamWithRole(parent, arg, ctx, info),
     teamsByOwner: (parent, arg, ctx, info) => teamsByOwner(parent, arg, ctx, info),
     teamsByRole: (parent, arg, ctx, info) => teamsByRole(parent, arg, ctx, info),

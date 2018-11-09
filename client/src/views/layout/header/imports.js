c4528d67c389d1bb404faf1329ca12caa0c99436
Unknown <nikolabjelo@gmail.com>
2018-11-09 18:21:47 +0100

    authentificated only links

--- client/src/views/layout/header/imports.js
+++ client/src/views/layout/header/imports.js
@@ -3,7 +3,6 @@ import {
   GamepadRounded,
   AccessibilityNew,
   LibraryAdd,
-  Home,
   ImportContacts,
   BugReport,
   People
@@ -13,11 +12,10 @@ const usersMenus = {
   title: 'Users',
   to: '/users',
   submenus: [
-    { title: 'Home', to: '/users/', icon: Home },
     { title: 'Directory', to: '/users/browse', icon: ImportContacts },
     { title: 'Search', to: '/users/search', icon: Search },
     { title: 'Report', to: 'https://github.com/AdvancedAlgos/UsersModule/issues/new', icon: BugReport, externalLink: true },
-    { title: 'Your Profile', to: '/users/user', icon: People }
+    { title: 'Your Profile', to: '/users/user', icon: People, authenticated: true }
   ]
 }
 
@@ -26,9 +24,9 @@ const teamsMenus = {
   to: '/teams',
   submenus: [
     { title: 'All teams', to: '/teams', icon: Search },
-    { title: 'Your teams', to: '/teams/manage-teams', icon: GamepadRounded },
-    { title: 'Team members', to: '/teams/team-members', icon: GamepadRounded },
-    { title: 'Financial beings', to: '/teams/financial-beings', icon: GamepadRounded },
+    { title: 'Your teams', to: '/teams/manage-teams', icon: GamepadRounded, authenticated: true },
+    { title: 'Team members', to: '/teams/team-members', icon: GamepadRounded, authenticated: true },
+    { title: 'Financial beings', to: '/teams/financial-beings', icon: GamepadRounded, authenticated: true },
     { title: 'Report', to: 'https://github.com/AdvancedAlgos/TeamsModule/issues/new', icon: LibraryAdd, externalLink: true }
   ]
 }
@@ -38,9 +36,9 @@ const eventsMenus = {
   to: '/events',
   submenus: [
     { title: 'All events', to: '/events', icon: Search },
-    { title: 'Your events', to: '/events/my', icon: GamepadRounded },
-    { title: 'Your hosted events', to: '/events/host', icon: AccessibilityNew },
-    { title: 'Host an event', to: '/events/create', icon: LibraryAdd },
+    { title: 'Your events', to: '/events/my', icon: GamepadRounded, authenticated: true },
+    { title: 'Your hosted events', to: '/events/host', icon: AccessibilityNew, authenticated: true },
+    { title: 'Host an event', to: '/events/create', icon: LibraryAdd, authenticated: true },
     { title: 'Report', to: 'https://github.com/AdvancedAlgos/UsersModule/issues/new', icon: LibraryAdd, externalLink: true }
   ]
 }
@@ -48,6 +46,7 @@ const eventsMenus = {
 const keyvaultMenus = {
   title: 'Key Vault',
   to: '/key-vault',
+  authenticated: true,
   submenus: [
     { title: 'Manage keys', to: '/key-vault/browse', icon: Search },
     { title: 'Add key', to: '/key-vault/addKey', icon: GamepadRounded },

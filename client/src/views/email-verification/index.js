b870d0d491ba25883f626f1bfa46c42bdb831330
Barry Low <barrylow@gmail.com>
2018-11-09 12:17:05 -0800

    Add key to email-verification text <br />

--- client/src/views/email-verification/index.js
+++ client/src/views/email-verification/index.js
@@ -5,7 +5,7 @@ import { BannerTopBar } from '../common'
 export const EmailSignupConfirm = () => <BannerTopBar
   size='big'
   title=''
-  text={['Processing newsletter sign-up...', <br />, 'Thank you for opting-in to recieve updates about the Advanced Algos project!']}
+  text={['Processing newsletter sign-up...', <br key='EmailSignupConfirm-br' />, 'Thank you for opting-in to recieve updates about the Advanced Algos project!']}
   backgroundUrl='https://aacorporatesitedevelop.azurewebsites.net/img/photos/superalgos-platform.jpg'
 />
 

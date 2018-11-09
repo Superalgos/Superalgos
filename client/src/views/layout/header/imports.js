8bcb09870007c9e4827c185a89ce6108620381ec
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-11-09 18:34:16 +0100

    Resolving Conflics

--- client/src/views/layout/header/imports.js
+++ client/src/views/layout/header/imports.js
@@ -24,17 +24,10 @@ const teamsMenus = {
   to: '/teams',
   submenus: [
     { title: 'All teams', to: '/teams', icon: Search },
-<<<<<<< HEAD
-    { title: 'Your teams', to: '/teams/manage-teams', icon: GamepadRounded },
-    { title: 'Team members', to: '/teams/team-members', icon: GamepadRounded },
-    { title: 'Financial beings', to: '/teams/financial-beings', icon: GamepadRounded },
-    { title: 'Report', to: 'https://github.com/AdvancedAlgos/TeamsModule/issues/new', icon: BugReport, externalLink: true }
-=======
     { title: 'Your teams', to: '/teams/manage-teams', icon: GamepadRounded, authenticated: true },
     { title: 'Team members', to: '/teams/team-members', icon: GamepadRounded, authenticated: true },
     { title: 'Financial beings', to: '/teams/financial-beings', icon: GamepadRounded, authenticated: true },
-    { title: 'Report', to: 'https://github.com/AdvancedAlgos/TeamsModule/issues/new', icon: LibraryAdd, externalLink: true }
->>>>>>> bb03bc2e61cf91e6f78adf00f500855414664268
+    { title: 'Report', to: 'https://github.com/AdvancedAlgos/TeamsModule/issues/new', icon: BugReport, externalLink: true }
   ]
 }
 
@@ -43,17 +36,10 @@ const eventsMenus = {
   to: '/events',
   submenus: [
     { title: 'All events', to: '/events', icon: Search },
-<<<<<<< HEAD
-    { title: 'Your events', to: '/events/my', icon: GamepadRounded },
-    { title: 'Your hosted events', to: '/events/host', icon: AccessibilityNew },
-    { title: 'Host an event', to: '/events/create', icon: LibraryAdd },
-    { title: 'Report', to: 'https://github.com/AdvancedAlgos/UsersModule/issues/new', icon: BugReport, externalLink: true }
-=======
     { title: 'Your events', to: '/events/my', icon: GamepadRounded, authenticated: true },
     { title: 'Your hosted events', to: '/events/host', icon: AccessibilityNew, authenticated: true },
     { title: 'Host an event', to: '/events/create', icon: LibraryAdd, authenticated: true },
-    { title: 'Report', to: 'https://github.com/AdvancedAlgos/UsersModule/issues/new', icon: LibraryAdd, externalLink: true }
->>>>>>> bb03bc2e61cf91e6f78adf00f500855414664268
+    { title: 'Report', to: 'https://github.com/AdvancedAlgos/UsersModule/issues/new', icon: BugReport, externalLink: true }
   ]
 }
 

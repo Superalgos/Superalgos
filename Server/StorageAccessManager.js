d20c8794c56ac48d684df82d5cc458815d91d79f
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2018-11-16 13:05:36 +0100

    Storage Permissions improved

--- Server/StorageAccessManager.js
+++ Server/StorageAccessManager.js
@@ -5,13 +5,26 @@
         initialize: initialize
     }
 
-    let permissions;
+    let connectionString;
 
     return thisObject;
 
-    function initialize(pServerConfig, callBackFunction) {
+    function initialize(serverConfig, callBackFunction) {
 
-        permissions = pServerConfig.storagePermissions;
+        switch (serverConfig.environment) {
+
+            case "Develop": {
+
+                connectionString = serverConfig.configAndPlugins.Develop.connectionString;
+                break;
+            }
+
+            case "Production": {
+
+                connectionString = serverConfig.configAndPlugins.Production.connectionString;
+                break;
+            }
+        }
 
         callBackFunction();
     }
@@ -19,7 +32,7 @@
     function getPermission(pContainer, pType, pDays) {
 
         let azure = require('azure-storage');
-        let blobService = azure.createBlobService(permissions.connectionString);
+        let blobService = azure.createBlobService(connectionString);
 
         let startDate = new Date();
         startDate.setDate(startDate.getDate() - 1);

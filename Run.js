ad567daba47221a720c65a6482f3a9dc256a71f0
mati108@gmail.com <34651529+matbenitez@users.noreply.github.com>
2018-11-17 13:52:09 -0300

    Adding dev team name to default containers

--- Run.js
+++ Run.js
@@ -88,7 +88,7 @@ function readStoragePermissions() {
 
             let permissionsMap = new Map;
 
-            let containers = ["AAPlatform", "AAVikings", global.USER_PROFILE.teamName]
+            let containers = ["AAPlatform", "AAVikings", global.DEV_TEAM]
             let container;
             let key;
             let value;

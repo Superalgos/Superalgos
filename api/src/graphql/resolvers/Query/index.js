766ed5e8ef34d14dac29dcd16ba70470d819eb8c
Barry Low <barrylow@gmail.com>
2019-02-15 09:02:16 -0800

    API: Add queries teamsByIds and teamAuthorization

--- api/src/graphql/resolvers/Query/index.js
+++ api/src/graphql/resolvers/Query/index.js
@@ -1,7 +1,7 @@
 import { logger } from '../../../logger'
 
 import { member, owner } from './Member'
-import { teams, teamById, teamByName, teamBySlug, teamWithRole, teamsByOwner, teamsByRole } from './Team'
+import { teams, teamById, teamsByIds, teamByName, teamBySlug, teamWithRole, teamAuthorization, teamsByOwner, teamsByRole } from './Team'
 import{ fbByTeamMember } from './TeamMember'
 
 export const resolvers = {
@@ -9,9 +9,11 @@ export const resolvers = {
     member: (parent, arg, ctx, info) => member(parent, arg, ctx, info),
     teams: (parent, arg, ctx, info) => teams(parent, arg, ctx, info),
     teamById: (parent, arg, ctx, info) => teamById(parent, arg, ctx, info),
+    teamsByIds: (parent, arg, ctx, info) => teamsByIds(parent, arg, ctx, info),
     teamByName: (parent, arg, ctx, info) => teamByName(parent, arg, ctx, info),
     teamBySlug: (parent, arg, ctx, info) => teamBySlug(parent, arg, ctx, info),
     teamWithRole: (parent, arg, ctx, info) => teamWithRole(parent, arg, ctx, info),
+    teamAuthorization: (parent, arg, ctx, info) => teamAuthorization(parent, arg, ctx, info),
     teamsByOwner: (parent, arg, ctx, info) => teamsByOwner(parent, arg, ctx, info),
     teamsByRole: (parent, arg, ctx, info) => teamsByRole(parent, arg, ctx, info),
     owner: (parent, arg, ctx, info) => owner(parent, arg, ctx, info),

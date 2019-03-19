edcda1a680d5b563c50df5ee7d85ad252f07ae22
Unknown <nikolabjelo@gmail.com>
2019-03-19 12:57:41 +0100

    Adding the cockpit to the masterApp

--- server/src/links/operations.js
+++ server/src/links/operations.js
@@ -6,9 +6,43 @@ export const linkSchemaDefs =
     extend type operations_GetClones {
       bot: teams_FinancialBeings
     }
+    extend type operations_Clone {
+      orders( state: cockpit_OrderStateEnum ): [cockpit_Order]
+    }
+    extend type operations_Clone {
+      signals( state: cockpit_SignalStateEnum ): [cockpit_Signal]
+    }
   `
 
-export const resolver = (teamsSchema) => ({
+export const resolver = (teamsSchema, cockpitSchema) => ({
+  operations_Clone: {
+    orders: {
+      fragment: `fragment OrderFragment on operations_Clone{id}`,
+      resolve ({id: cloneId}, args, context, info) {
+        return info.mergeInfo.delegateToSchema({
+          schema: cockpitSchema,
+          operation: 'query',
+          fieldName: 'cockpit_OrdersByClondeId',
+          args: { ...args, cloneId },
+          context,
+          info
+        })
+      }
+    },
+    signals: {
+      fragment: `fragment SignalFragment on operations_Clone{id}`,
+      resolve ({id: cloneId}, args, context, info) {
+        return info.mergeInfo.delegateToSchema({
+          schema: cockpitSchema,
+          operation: 'query',
+          fieldName: 'cockpit_SignalsByClondeId',
+          args: { ...args, cloneId },
+          context,
+          info
+        })
+      }
+    }
+  },
   operations_GetClones: {
     team: {
       fragment: `fragment TeamFragment on operations_GetClones{teamId}`,

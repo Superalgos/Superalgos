82604cdf4ca1162c1aac4fbd0f6cf13dba0ac3c1
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2019-03-22 17:24:28 +0100

    Saira

--- BollingerBandsPanel.js
+++ BollingerBandsPanel.js
@@ -194,7 +194,7 @@ function newAAMastersPlottersBollingerBandsBollingerBandsBollingerBandsPanel () 
             let labelPoint;
             let fontSize = 10;
 
-            browserCanvasContext.font = fontSize + 'px ' + UI_FONT.SECONDARY;
+            browserCanvasContext.font = fontSize + 'px ' + UI_FONT.SECONDARY + ' Saira';
 
             let label = '' + labelToPrint;
             if (isNaN(label) === false) {

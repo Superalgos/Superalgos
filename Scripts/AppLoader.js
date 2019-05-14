a276d439da31f682a5a3b9b8d53a434eb6c5abb6
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2019-05-14 16:14:17 +0200

    Refactoring to Cockpit Space

--- Scripts/AppLoader.js
+++ Scripts/AppLoader.js
@@ -51,7 +51,7 @@ function newAppLoader () {
         'ControlsToolBox/SidePanel.js',
         'ControlsToolBox/SidePanelTab.js',
 
-        'Spaces/BottomSpace.js',
+        'Spaces/CockpitSpace.js',
         'Spaces/TopSpace.js',
         'Spaces/PanelsSpace.js',
         'Spaces/ChartSpace.js',
@@ -90,7 +90,7 @@ function newAppLoader () {
         'ChartsSpace/ViewPort.js',
         'ChartsSpace/TimeLineCoordinateSystem.js',
 
-        'BottomSpace/UIControl.js',
+        'CockpitSpace/UIControl.js',
 
         'Plotter.js',
         'PlotterPanel.js',

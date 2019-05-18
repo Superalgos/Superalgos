49e180bcfe9e46be671d316c33557b966a2c0fbb
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2019-05-18 12:47:51 +0200

    Removing LOG INFO

--- ProductStorage.js
+++ ProductStorage.js
@@ -1,7 +1,6 @@
  ﻿
 function newProductStorage (pName) {
   const MODULE_NAME = 'Product Storage'
-  const INFO_LOG = false
   const ERROR_LOG = true
   const logger = newWebDebugLog()
   logger.fileName = MODULE_NAME
@@ -54,8 +53,6 @@ function newProductStorage (pName) {
 
   function finalize () {
     try {
-      if (INFO_LOG === true) { logger.write('[INFO] finalize -> Entering function.') }
-
       for (let i = 0; i < thisObject.marketFiles.length; i++) {
         let marketFile = thisObject.marketFiles[i]
         marketFile.finalize()
@@ -83,28 +80,18 @@ function newProductStorage (pName) {
 
   function initialize (pDevTeam, pBot, pProduct, pExchange, pMarket, pDatetime, pTimePeriod, callBackFunction) {
     try {
-      if (INFO_LOG === true) { logger.write('[INFO] initialize -> Entering function.') }
-      if (INFO_LOG === true) { logger.write('[INFO] initialize -> key = ' + pDevTeam.codeName + '-' + pBot.codeName + '-' + pProduct.codeName) }
-
       datetime = pDatetime
       timePeriod = pTimePeriod
 
       let dataSetsToLoad = 0
       let dataSetsLoaded = 0
 
-      if (INFO_LOG === true) { logger.write('[INFO] initialize -> dataSetsToLoad = ' + dataSetsToLoad) }
-      if (INFO_LOG === true) { logger.write('[INFO] initialize -> dataSetsLoaded = ' + dataSetsLoaded) }
-
       for (let i = 0; i < pProduct.dataSets.length; i++) {
         let thisSet = pProduct.dataSets[i]
 
         switch (thisSet.type) {
           case 'Market Files': {
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> Market Files -> key = ' + pDevTeam.codeName + '-' + pBot.codeName + '-' + pProduct.codeName) }
-
             dataSetsToLoad++
-
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> dataSetsToLoad = ' + dataSetsToLoad) }
 
             let marketFiles = newMarketFiles()
             marketFiles.initialize(pDevTeam, pBot, pProduct, thisSet, pExchange, pMarket, onMarketFileReady)
@@ -113,11 +100,7 @@ function newProductStorage (pName) {
             break
 
           case 'Daily Files': {
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> Daily Files -> key = ' + pDevTeam.codeName + '-' + pBot.codeName + '-' + pProduct.codeName) }
-
             dataSetsToLoad++
-
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> dataSetsToLoad = ' + dataSetsToLoad) }
 
             let dailyFiles = newDailyFiles()
             dailyFiles.initialize(pDevTeam, pBot, pProduct, thisSet, pExchange, pMarket, pDatetime, pTimePeriod, onDailyFileReady)
@@ -126,11 +109,7 @@ function newProductStorage (pName) {
             break
 
           case 'Single File': {
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> Single File -> key = ' + pDevTeam.codeName + '-' + pBot.codeName + '-' + pProduct.codeName) }
-
             dataSetsToLoad++
-
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> dataSetsToLoad = ' + dataSetsToLoad) }
 
             let singleFile = newSingleFile()
             singleFile.initialize(pDevTeam, pBot, pProduct, thisSet, pExchange, pMarket, onSingleFileReady)
@@ -139,11 +118,7 @@ function newProductStorage (pName) {
             break
 
           case 'File Sequence': {
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> File Sequence -> key = ' + pDevTeam.codeName + '-' + pBot.codeName + '-' + pProduct.codeName) }
-
             dataSetsToLoad++
-
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> dataSetsToLoad = ' + dataSetsToLoad) }
 
             let fileSequences = newFileSequence()
             fileSequences.initialize(pDevTeam, pBot, pProduct, thisSet, pExchange, pMarket, onFileSequenceReady)
@@ -154,31 +129,22 @@ function newProductStorage (pName) {
 
         function onMarketFileReady (err, pCaller) {
           try {
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onMarketFileReady -> Entering function.') }
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onMarketFileReady -> key = ' + pDevTeam.codeName + '-' + pBot.codeName + '-' + pProduct.codeName) }
-
             switch (err.result) {
               case GLOBAL.DEFAULT_OK_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onMarketFileReady -> Received OK Response.') }
                 break
               }
 
               case GLOBAL.DEFAULT_FAIL_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onMarketFileReady -> Received FAIL Response.') }
                 callBackFunction(GLOBAL.DEFAULT_FAIL_RESPONSE)
                 return
               }
 
               case GLOBAL.CUSTOM_FAIL_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onMarketFileReady -> Received CUSTOM FAIL Response.') }
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onMarketFileReady -> err= ' + err.stack) }
-
                 callBackFunction(err)
                 return
               }
 
               default: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onMarketFileReady -> Received Unexpected Response.') }
                 callBackFunction(err)
                 return
               }
@@ -192,49 +158,34 @@ function newProductStorage (pName) {
 
             thisObject.eventHandler.raiseEvent('Market File Loaded', event)
 
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onMarketFileReady -> event.currentValue = ' + event.currentValue) }
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onMarketFileReady -> event.totalValue = ' + event.totalValue) }
-
             if (event.currentValue + event.filesNotLoaded === event.totalValue) {
               dataSetsLoaded++
-
-              if (INFO_LOG === true) { logger.write('[INFO] initialize -> onMarketFileReady -> dataSetsLoaded = ' + dataSetsLoaded) }
 
               checkInitializeComplete()
             }
           } catch (err) {
-            if (ERROR_LOG === true) { logger.write('[ERROR] initialize -> onMarketFileReady -> err = ' + err.stack) }
             callBackFunction(GLOBAL.DEFAULT_FAIL_RESPONSE)
           }
         }
 
         function onDailyFileReady (err, pCaller) {
           try {
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onDailyFileReady -> Entering function.') }
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onDailyFileReady -> key = ' + pDevTeam.codeName + '-' + pBot.codeName + '-' + pProduct.codeName) }
-
             switch (err.result) {
               case GLOBAL.DEFAULT_OK_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onDailyFileReady -> Received OK Response.') }
                 break
               }
 
               case GLOBAL.DEFAULT_FAIL_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onDailyFileReady -> Received FAIL Response.') }
                 callBackFunction(GLOBAL.DEFAULT_FAIL_RESPONSE)
                 return
               }
 
               case GLOBAL.CUSTOM_FAIL_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onDailyFileReady -> Received CUSTOM FAIL Response.') }
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onDailyFileReady -> err= ' + err.stack) }
-
                 callBackFunction(err)
                 return
               }
 
               default: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onDailyFileReady -> Received Unexpected Response.') }
                 callBackFunction(err)
                 return
               }
@@ -247,49 +198,33 @@ function newProductStorage (pName) {
 
             thisObject.eventHandler.raiseEvent('Daily File Loaded', event)
 
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onDailyFileReady -> event.currentValue = ' + event.currentValue) }
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onDailyFileReady -> event.totalValue = ' + event.totalValue) }
-
             if (event.currentValue === event.totalValue) {
               dataSetsLoaded++
-
-              if (INFO_LOG === true) { logger.write('[INFO] initialize -> onDailyFileReady -> dataSetsLoaded = ' + dataSetsLoaded) }
-
               checkInitializeComplete()
             }
           } catch (err) {
-            if (ERROR_LOG === true) { logger.write('[ERROR] initialize -> onDailyFileReady -> err = ' + err.stack) }
             callBackFunction(GLOBAL.DEFAULT_FAIL_RESPONSE)
           }
         }
 
         function onSingleFileReady (err, pCaller) {
           try {
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onSingleFileReady -> Entering function.') }
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onSingleFileReady -> key = ' + pDevTeam.codeName + '-' + pBot.codeName + '-' + pProduct.codeName) }
-
             switch (err.result) {
               case GLOBAL.DEFAULT_OK_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onSingleFileReady -> Received OK Response.') }
                 break
               }
 
               case GLOBAL.DEFAULT_FAIL_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onSingleFileReady -> Received FAIL Response.') }
                 callBackFunction(GLOBAL.DEFAULT_FAIL_RESPONSE)
                 return
               }
 
               case GLOBAL.CUSTOM_FAIL_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onSingleFileReady -> Received CUSTOM FAIL Response.') }
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onSingleFileReady -> err= ' + err.stack) }
-
                 callBackFunction(err)
                 return
               }
 
               default: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onSingleFileReady -> Received Unexpected Response.') }
                 callBackFunction(err)
                 return
               }
@@ -304,44 +239,31 @@ function newProductStorage (pName) {
 
             if (event.currentValue === event.totalValue) {
               dataSetsLoaded++
-
-              if (INFO_LOG === true) { logger.write('[INFO] initialize -> onSingleFileReady -> dataSetsLoaded = ' + dataSetsLoaded) }
-
               checkInitializeComplete()
             }
           } catch (err) {
-            if (ERROR_LOG === true) { logger.write('[ERROR] initialize -> onSingleFileReady -> err = ' + err.stack) }
             callBackFunction(GLOBAL.DEFAULT_FAIL_RESPONSE)
           }
         }
 
         function onFileSequenceReady (err, pCaller) {
           try {
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onFileSequenceReady -> Entering function.') }
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> onFileSequenceReady -> key = ' + pDevTeam.codeName + '-' + pBot.codeName + '-' + pProduct.codeName) }
-
             switch (err.result) {
               case GLOBAL.DEFAULT_OK_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onFileSequenceReady -> Received OK Response.') }
                 break
               }
 
               case GLOBAL.DEFAULT_FAIL_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onFileSequenceReady -> Received FAIL Response.') }
                 callBackFunction(GLOBAL.DEFAULT_FAIL_RESPONSE)
                 return
               }
 
               case GLOBAL.CUSTOM_FAIL_RESPONSE.result: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onFileSequenceReady -> Received CUSTOM FAIL Response.') }
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onFileSequenceReady -> err= ' + err.stack) }
-
                 callBackFunction(err)
                 return
               }
 
               default: {
-                if (INFO_LOG === true) { logger.write('[INFO] initialize -> onFileSequenceReady -> Received Unexpected Response.') }
                 callBackFunction(err)
                 return
               }
@@ -356,25 +278,15 @@ function newProductStorage (pName) {
 
             if (event.currentValue === event.totalValue) {
               dataSetsLoaded++
-
-              if (INFO_LOG === true) { logger.write('[INFO] initialize -> onFileSequenceReady = ' + dataSetsLoaded) }
-
               checkInitializeComplete()
             }
           } catch (err) {
-            if (ERROR_LOG === true) { logger.write('[ERROR] initialize -> onFileSequenceReady -> err = ' + err.stack) }
             callBackFunction(GLOBAL.DEFAULT_FAIL_RESPONSE)
           }
         }
 
         function checkInitializeComplete () {
           try {
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> checkInitializeComplete -> Entering function.') }
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> checkInitializeComplete -> key = ' + pDevTeam.codeName + '-' + pBot.codeName + '-' + pProduct.codeName) }
-
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> checkInitializeComplete -> dataSetsToLoad = ' + dataSetsToLoad) }
-            if (INFO_LOG === true) { logger.write('[INFO] initialize -> checkInitializeComplete -> dataSetsLoaded = ' + dataSetsLoaded) }
-
             if (dataSetsLoaded === dataSetsToLoad) {
               callBackFunction(GLOBAL.DEFAULT_OK_RESPONSE)
             }
@@ -391,8 +303,6 @@ function newProductStorage (pName) {
   }
 
   function setDatetime (pDatetime) {
-    if (INFO_LOG === true) { logger.write('[INFO] setDatetime -> Entering function.') }
-
         /* If there is a change in the day, then we take some actions, otherwise, we dont. */
 
     let currentDate = Math.trunc(datetime.valueOf() / ONE_DAY_IN_MILISECONDS)
@@ -410,8 +320,6 @@ function newProductStorage (pName) {
   }
 
   function setTimePeriod (pTimePeriod) {
-    if (INFO_LOG === true) { logger.write('[INFO] setTimePeriod -> Entering function.') }
-
         /* We are going to filter out the cases in which the timePeriod received is the same that the one we already know. */
 
     if (timePeriod !== pTimePeriod) {

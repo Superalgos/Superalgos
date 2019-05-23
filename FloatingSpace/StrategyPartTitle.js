d54f4bcdd6f5c7359523689ba892e4ad27c97371
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2019-05-23 10:10:28 +0200

    Back to displaying long titles

--- FloatingSpace/StrategyPartTitle.js
+++ FloatingSpace/StrategyPartTitle.js
@@ -252,7 +252,7 @@ function newStrategyPartTitle () {
   }
 
   function trimTitle (title) {
-    const MAX_LABEL_LENGTH = 15
+    const MAX_LABEL_LENGTH = 35
     if (title.length > MAX_LABEL_LENGTH) {
       title = title.substring(0, MAX_LABEL_LENGTH) + '...'
     }

7c01cd19a38f8d2b53fade39d56e0db9bb611993
Matias <mati108@gmail.com>
2019-05-24 20:28:36 -0300

    Removing sessionToken

--- TopSpace/CurrentEvent.js
+++ TopSpace/CurrentEvent.js
@@ -86,9 +86,9 @@ function newCurrentEvent () {
   return thisObject
 
   function initialize (pSharedStatus) {
-    let sessionToken = window.localStorage.getItem('sessionToken')
+    let accessToken = window.localStorage.getItem('access_token')
 
-    if (sessionToken === null || sessionToken === '') {
+    if (accessToken === null || accessToken === '') {
             /* not logged in */
       return
     }
@@ -126,9 +126,9 @@ function newCurrentEvent () {
   }
 
   function onClick () {
-    let sessionToken = window.localStorage.getItem('sessionToken')
+    let accessToken = window.localStorage.getItem('access_token')
 
-    if (sessionToken === null || sessionToken === '') {
+    if (accessToken === null || accessToken === '') {
             /* not logged in */
       return
     }
@@ -169,9 +169,9 @@ function newCurrentEvent () {
   }
 
   function draw () {
-    let sessionToken = window.localStorage.getItem('sessionToken')
+    let accessToken = window.localStorage.getItem('access_token')
 
-    if (sessionToken === null || sessionToken === '') {
+    if (accessToken === null || accessToken === '') {
             /* not logged in */
       return
     }

9a6b8a1572ae2e4c754bdbde528ee0da8a8d1ad9
Matias <mati108@gmail.com>
2019-07-03 20:10:19 -0300

    Add and remove bot to ecosystem

--- src/schema/mutations/index.js
+++ src/schema/mutations/index.js
@@ -5,13 +5,17 @@ import {
 import AddTeamMutation from './AddTeam'
 import DeleteTeamMutation from './DeleteTeam'
 import CreateFileMutation from './CreateFile'
+import AddBotMutation from './AddBot'
+import RemoveBotMutation from './RemoveBot'
 
 const Mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: Object.assign(
     AddTeamMutation,
     DeleteTeamMutation,
-    CreateFileMutation
+    CreateFileMutation,
+    AddBotMutation,
+    RemoveBotMutation
   )
 })
 
--- src/schema/types/input/index.js
+++ src/schema/types/input/index.js
@@ -3,11 +3,19 @@ import BotInputType from './Bot'
 import HostInputType from './Host'
 import FileInputType from './File'
 import DeleteTeamInputType from './DeleteTeam'
+import DataSetInputType from './DataSet'
+import ExchangeInputType from './Exchange'
+import PlotterInputType from './Plotter'
+import ProductInputType from './Product'
 
 export {
   TeamInputType,
   BotInputType,
   HostInputType,
   FileInputType,
-  DeleteTeamInputType
+  DeleteTeamInputType,
+  DataSetInputType,
+  ExchangeInputType,
+  PlotterInputType,
+  ProductInputType
 }

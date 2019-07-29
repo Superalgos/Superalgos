2020087080117f2721db5b5e6dd8073ae0bf1124
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2019-07-29 17:55:44 +0200

    Better Logging

--- WebDebugLog.js
+++ WebDebugLog.js
@@ -1,17 +1,21 @@
  function newWebDebugLog () {
-  let thisObject = {
-    write: write,
-    fileName: 'undefined'
-  }
+   try {
+     let thisObject = {
+       write: write,
+       fileName: 'undefined'
+     }
 
-  return thisObject
+     return thisObject
 
-  function write (pText) {
-    console.log(spacePad(thisObject.fileName, 50) + ' : ' + pText)
+     function write (pText) {
+       console.log(spacePad(thisObject.fileName, 50) + ' : ' + pText)
 
-    function spacePad (str, max) {
-      str = str.toString()
-      return str.length < max ? spacePad(' ' + str, max) : str
-    }
-  }
-}
+       function spacePad (str, max) {
+         str = str.toString()
+         return str.length < max ? spacePad(' ' + str, max) : str
+       }
+     }
+   } catch (err) {
+     console.log(spacePad(MODULE_NAME, 50) + ' : ' + '[ERROR]  WebDebugLog --> err = ' + err.stack)
+   }
+ }

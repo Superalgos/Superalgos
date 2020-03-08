ec996bd41ba40aa7509e74b80ec331e189e5cbe1
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-03-08 18:17:58 +0100

    Improvements on layer panels

--- ChartingSpace/TimeMachine.js
+++ ChartingSpace/TimeMachine.js
@@ -417,7 +417,10 @@ function newTimeMachine () {
   }
 
   function panelPhysics () {
-    if (thisObject.container.frame.isCenterInViewPort() && canvas.chartingSpace.viewport.zoomTargetLevel >= ZOOM_OUT_THRESHOLD_FOR_HIDDING_PANELS) {
+    if (
+      thisObject.container.frame.isCenterInViewPort() &&
+      canvas.chartingSpace.viewport.zoomLevel >= ZOOM_OUT_THRESHOLD_FOR_HIDDING_PANELS
+    ) {
       canvas.panelsSpace.unHide(thisObject.payload.node.id, 'Layers Panel')
       canvas.panelsSpace.unHide(thisObject.payload.node.id, 'Plotter Panel')
     } else {

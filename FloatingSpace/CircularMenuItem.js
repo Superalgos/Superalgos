9e46c9296ed9e6c6706c54b7a1c8b1d931a2644a
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-03-10 15:45:47 +0100

    fixed

--- FloatingSpace/CircularMenuItem.js
+++ FloatingSpace/CircularMenuItem.js
@@ -112,7 +112,7 @@ function newCircularMenuItem () {
     selfMouseNotOverEventSubscriptionId = thisObject.container.eventHandler.listenToEvent('onMouseNotOver', onMouseNotOver)
 
     if (thisObject.type === 'Icon & Text') {
-      thisObject.container.frame.width = 200
+      thisObject.container.frame.width = 220
     } else {
       thisObject.container.frame.width = 50
     }

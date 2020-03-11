92a4f5a50492d3c600e0f084caa2990a22026fc6
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-03-11 14:20:44 +0100

    Directory Structure Changed

--- src/FileCloud.js
+++ src/FileCloud.js
@@ -75,8 +75,8 @@ function newFileCloud () {
           }
 
           if (pMarket !== undefined) {
-            fileName = fileName.replace('@BaseAsset', pMarket.baseAsset)
-            fileName = fileName.replace('@QuotedAsset', pMarket.quotedAsset)
+            filePath = filePath.replace('@BaseAsset', pMarket.baseAsset)
+            filePath = filePath.replace('@QuotedAsset', pMarket.quotedAsset)
           }
 
           if (pDataMine !== undefined) {

a174ae009c5c4c0557d80d634bb8e871d84e2ebc
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-03-26 15:07:30 +0100

    Update SessionFunctions.js

--- DesignSpace/Workspace/FunctionLibraries/SessionFunctions.js
+++ DesignSpace/Workspace/FunctionLibraries/SessionFunctions.js
@@ -54,7 +54,8 @@ function newSessionFunctions () {
     'Backtesting Session->Paper Trading Session->Fordward Testing Session->Live Trading Session->' +
     'Parameters->' +
     'Base Asset->Quoted Asset->Time Range->Time Frame->Slippage->Fee Structure->' +
-    'Exchange Account Asset->Asset->'
+    'Exchange Account Asset->Asset->' +
+    'Social Bots->Telegram Bot->'
 
     let session = functionLibraryProtocolNode.getProtocolNode(node, false, true, true, false, false, lightingPath)
 

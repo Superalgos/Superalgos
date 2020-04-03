a37da2a044b2379c38176075467ae0519c25f28a
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-04-03 12:44:38 +0200

    Update UiObjectsFromNodes.js

--- DesignSpace/Workspace/FunctionLibraries/UiObjectsFromNodes.js
+++ DesignSpace/Workspace/FunctionLibraries/UiObjectsFromNodes.js
@@ -24,9 +24,9 @@ function newUiObjectsFromNodes () {
     function addIncludedNodes () {
       blobService = newFileStorage()
 
-      if (node.code === undefined) {
-        node.code = '{ \n"includeDataMines": ["Masters", "Sparta", "TradingEngines"],\n"includeTradingSystems": ["Sparta-WHB-BTC-USDT", "Masters-WHB-ETH-USDT", "Sparta-BRR-BTC-USDT"],\n"includeSuperScripts": ["Masters"]\n }'
-      }
+      // if (node.code === undefined) {
+      node.code = '{ \n"includeDataMines": ["Masters", "Sparta", "TradingEngines"],\n"includeTradingSystems": ["Sparta-WHB-BTC-USDT", "Masters-WHB-ETH-USDT", "Sparta-BRR-BTC-USDT"],\n"includeSuperScripts": ["Masters"]\n }'
+      // }
 
       let code = JSON.parse(node.code)
       let includeDataMines = code.includeDataMines

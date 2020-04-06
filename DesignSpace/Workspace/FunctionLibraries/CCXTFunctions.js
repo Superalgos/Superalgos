010eb302093d6412b1efd345a48e968fd875acda
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-04-06 18:01:15 +0200

    Update CCXTFunctions.js

--- DesignSpace/Workspace/FunctionLibraries/CCXTFunctions.js
+++ DesignSpace/Workspace/FunctionLibraries/CCXTFunctions.js
@@ -115,9 +115,6 @@ function newCCXTFunctions () {
             let newAsseet = functionLibraryUiObjectsFromNodes.addUIObject(node, 'Asset')
             newAsseet.name = name
             newAsseet.code = '{ \n\"codeName\": \"' + name + '\"\n}'
-            newAsseet.payload.floatingObject.angleToParent = ANGLE_TO_PARENT.RANGE_360
-            newAsseet.payload.floatingObject.distanceToParent = DISTANCE_TO_PARENT.PARENT_200X
-            newAsseet.payload.floatingObject.arrangementStyle = ARRANGEMENT_STYLE.CONCAVE
           }
         }
       }

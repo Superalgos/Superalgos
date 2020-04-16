bd80848dc3662fb2be1f0b270683a6303d67202a
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-04-16 11:06:24 +0200

    Web Server Integrated to Backend Servers

---
+++ BackendServers/WebServer/LegacyPlotter.js
@@ -0,0 +1,6 @@
+﻿function getNewPlotter(pDataMineOrHost, pPlotter, pModule) {
+    let functionName = pDataMineOrHost + pPlotter + pModule;
+    functionName = functionName.replace(/-/g, "");
+    functionName = 'new' + functionName
+    return window[functionName]();
+}

5fc32f9afde42bcb9a74c6c785cd9d6816309dfa
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-09-02 18:47:38 +0200

    Values

--- FloatingSpace/UiObject.js
+++ FloatingSpace/UiObject.js
@@ -26,6 +26,7 @@ function newUiObject() {
         hasError: undefined,
         hasWarning: undefined,
         hasInfo: undefined,
+        valueAtAngle: true,
         run: run,
         stop: stop,
         heartBeat: heartBeat,
@@ -1658,21 +1659,17 @@ function newUiObject() {
                     fontSize = fontSize * 2 / 3
                 }
 
-                labelPoint = {
-                    x: position.x - label.length / 2 * fontSize * FONT_ASPECT_RATIO - 10,
-                    y: position.y
-                }
-
-                /* We will try to put this value according to the angle to the parent node*/
-                if (thisObject.payload !== undefined) {
-                    if (thisObject.payload.angle !== undefined) {
-                        labelPoint.x = labelPoint.x + radius * 7 / 3 * Math.cos(toRadians(thisObject.payload.angle))
-                        labelPoint.y = labelPoint.y + radius * 7 / 3 * Math.sin(toRadians(thisObject.payload.angle))
-                    } else {
-                        labelPoint = {
-                            x: position.x - label.length / 2 * fontSize * FONT_ASPECT_RATIO - 10,
-                            y: position.y  + radius * 7 / 5 + fontSize * FONT_ASPECT_RATIO + 15
-                        }        
+                if (thisObject.valueAtAngle === true && thisObject.payload.angle !== undefined) {
+                    labelPoint = {
+                        x: position.x - label.length / 2 * fontSize * FONT_ASPECT_RATIO - 10,
+                        y: position.y
+                    }
+                    labelPoint.x = labelPoint.x + radius * 7 / 3 * Math.cos(toRadians(thisObject.payload.angle))
+                    labelPoint.y = labelPoint.y + radius * 7 / 3 * Math.sin(toRadians(thisObject.payload.angle))
+                } else {
+                    labelPoint = {
+                        x: position.x - label.length / 2 * fontSize * FONT_ASPECT_RATIO - 10,
+                        y: position.y + radius * 7 / 5 + fontSize * FONT_ASPECT_RATIO + 15
                     }
                 }
 

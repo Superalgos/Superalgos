45afa8ccfd32c05f6bf365dddc3f9b8a16a87780
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-09-10 22:43:30 +0200

    minMemo

--- run.js
+++ run.js
@@ -1,7 +1,13 @@
-
 let options = {
     execArgv: ['--max-old-space-size=8192'],
     stdio: 'inherit'
+}
+
+if (process.argv.includes("minMemo")) {
+    options = {
+        stdio: 'inherit'
+    }
+    console.log('Running with Minimun Memory Required.')
 }
 
 const { fork } = require('child_process')

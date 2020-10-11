b5444d6c77ddd8982db9a18de14b6a39860aba20
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-10-11 16:11:42 +0200

    Masters Indicators Upgrade

--- Untitled-10.js
+++ Untitled-10.js
@@ -1,97 +1,184 @@
-let candle = record.current // This is the primary dependency
-let volume = getElement('volumes', candle) // Here I get the volume bar corresponding to the current candle
-let totalVolume = volume.buy + volume.sell // Candles Volumes split the volume received from the exchange evenly betwen buy and sell because it does not know how much of it there is.
 
-variable.buyVolume = 0
-variable.sellVolume = 0
 
-if (candle.direction === 'Up') {
-    variable.buyVolume = totalVolume
-}
-if (candle.direction === 'Down') {
-    variable.sellVolume = totalVolume
-}
-let totalBuy
-let totalSell
-let totalNonZero
-/* Maintain the 20 periods moving averages. */
-variable.last20Buy.splice(0, 1)
-variable.last20Buy.push(variable.buyVolume)
-totalBuy = 0
-totalNonZero = 0
-for (let i = 0; i < variable.last20Buy.length; i++) {
-    let value = variable.last20Buy[i]
-    if (value > 0) {
-        totalBuy = totalBuy + value
-        totalNonZero++
+let candle = record.current
+let previousCandle = record.previous
+
+variable.supportResistanceZone = calculateSupportResistanceZone()
+
+deleteResistances(candle.max)
+increasePeriods()
+
+variable.resistance = {}
+
+variable.resistance.rate = 0
+variable.resistance.period = 0
+variable.resistance.type = ''
+
+if (previousCandle !== undefined) {
+
+    let previousPreviousCandle = record.previous.previous
+
+    if (previousPreviousCandle !== undefined) {
+
+        if (previousCandle.max > previousPreviousCandle.max && previousCandle.max > candle.max) {
+            variable.resistance.rate = previousCandle.max
+            variable.resistance.period = 0
+            variable.resistance.type = 'Resistance'
+            increaseResistancesBounces(variable.resistance.rate)
+            rememberResistance(variable.resistance.rate)
+        }
     }
 }
-variable.buySMA20 = totalBuy / totalNonZero
 
-variable.last20Sell.splice(0, 1)
-variable.last20Sell.push(variable.sellVolume)
-totalSell = 0
-totalNonZero = 0
-for (let i = 0; i < variable.last20Sell.length; i++) {
-    let value = variable.last20Sell[i]
-    if (value > 0) {
-        totalSell = totalSell + value
-        totalNonZero++
+setClosestResistances()
+
+function deleteResistances(rate) {
+
+    let max
+
+    for (let i = 0; i < variable.resistances.length; i++) {
+        let currenRecord = variable.resistances[i]
+        let currentRate = currenRecord[0]
+
+        if (rate >= currentRate + currentRate * variable.supportResistanceZone / 2 / 100) {  // It must go through the support / resistance zone around the current rate value
+            max = i
+        }
+    }
+    if (max !== undefined) {
+        variable.resistances.splice(0, max + 1)
     }
 }
-variable.sellSMA20 = totalSell / totalNonZero
 
-/* Maintain the 200 periods moving averages. */
-variable.last200Buy.splice(0, 1)
-variable.last200Buy.push(variable.buyVolume)
-totalBuy = 0
-totalNonZero = 0
-for (let i = 0; i < variable.last200Buy.length; i++) {
-    let value = variable.last200Buy[i]
-    if (value > 0) {
-        totalBuy = totalBuy + value
-        totalNonZero++
+function increasePeriods() {
+
+    for (let i = 0; i < variable.resistances.length; i++) {
+        let currenRecord = variable.resistances[i]
+        currenRecord[1] = currenRecord[1] + 1
     }
 }
-variable.buySMA200 = totalBuy / totalNonZero
 
-variable.last200Sell.splice(0, 1)
-variable.last200Sell.push(variable.sellVolume)
-totalSell = 0
-totalNonZero = 0
-for (let i = 0; i < variable.last200Sell.length; i++) {
-    let value = variable.last200Sell[i]
-    if (value > 0) {
-        totalSell = totalSell + value
-        totalNonZero++
+function setClosestResistances() {
+    let record
+
+    record = variable.resistances[0]
+    if (record === undefined) { record = [0, 0, 0, 0, 0, 0, 0, 0] }
+    variable.resistance1Rate = record[0]
+    variable.resistance1Period = record[1]
+    variable.resistance1Bounce1 = record[2]
+    variable.resistance1Bounce2 = record[3]
+    variable.resistance1Bounce3 = record[4]
+    variable.resistance1Bounce5 = record[5]
+    variable.resistance1Bounce10 = record[6]
+    variable.resistance1BounceAll = record[7]
+
+    record = variable.resistances[1]
+    if (record === undefined) { record = [0, 0, 0, 0, 0, 0, 0, 0] }
+    variable.resistance2Rate = record[0]
+    variable.resistance2Period = record[1]
+    variable.resistance2Bounce1 = record[2]
+    variable.resistance2Bounce2 = record[3]
+    variable.resistance2Bounce3 = record[4]
+    variable.resistance2Bounce5 = record[5]
+    variable.resistance2Bounce10 = record[6]
+    variable.resistance2BounceAll = record[7]
+
+    record = variable.resistances[2]
+    if (record === undefined) { record = [0, 0, 0, 0, 0, 0, 0, 0] }
+    variable.resistance3Rate = record[0]
+    variable.resistance3Period = record[1]
+    variable.resistance3Bounce1 = record[2]
+    variable.resistance3Bounce2 = record[3]
+    variable.resistance3Bounce3 = record[4]
+    variable.resistance3Bounce5 = record[5]
+    variable.resistance3Bounce10 = record[6]
+    variable.resistance3BounceAll = record[7]
+
+    record = variable.resistances[3]
+    if (record === undefined) { record = [0, 0, 0, 0, 0, 0, 0, 0] }
+    variable.resistance4Rate = record[0]
+    variable.resistance4Period = record[1]
+    variable.resistance4Bounce1 = record[2]
+
+    record = variable.resistances[4]
+    if (record === undefined) { record = [0, 0, 0, 0, 0, 0, 0, 0] }
+    variable.resistance5Rate = record[0]
+    variable.resistance5Period = record[1]
+    variable.resistance5Bounce1 = record[2]
+
+}
+
+function rememberResistance(rate) {
+    if (variable.resistances.length === 0) {
+        variable.resistances.push([rate, 1, 0, 0, 0, 0, 0, 0])
+        return
+    }
+    for (let i = 0; i < variable.resistances.length; i++) {
+        let currenRecord = variable.resistances[i]
+        let currentRate = currenRecord[0]
+
+        if (rate > currentRate - currentRate * variable.supportResistanceZone / 2 / 100 && rate < currentRate + currentRate * variable.supportResistanceZone / 2 / 100) {
+            // We are not going to remember this new level since it is absorved by this existing one.
+            return
+        }
+
+        if (rate < currentRate) {
+            variable.resistances.splice(i, 0, [rate, 1, 0, 0, 0, 0, 0, 0])
+            return
+        }
+
+        if (i === variable.resistances.length - 1) {
+            variable.resistances.push([rate, 1, 0, 0, 0, 0, 0, 0])
+            return
+        }
     }
 }
-variable.sellSMA200 = totalSell / totalNonZero
 
+function increaseResistancesBounces(rate) {
 
+    for (let i = 0; i < variable.resistances.length; i++) {
+        let currenRecord = variable.resistances[i]
+        let currentRate = currenRecord[0]
+        let percentage = 100 - rate * 100 / currentRate
 
+        if (percentage <= variable.supportResistanceZone / 2 * 1) {
+            currenRecord[2] = currenRecord[2] + 1
+        }
 
+        if (percentage <= variable.supportResistanceZone / 2 * 2) {
+            currenRecord[3] = currenRecord[3] + 1
+        }
 
+        if (percentage <= variable.supportResistanceZone / 2 * 3) {
+            currenRecord[4] = currenRecord[4] + 1
+        }
 
+        if (percentage <= variable.supportResistanceZone / 2 * 5) {
+            currenRecord[5] = currenRecord[5] + 1
+        }
 
+        if (percentage <= variable.supportResistanceZone / 2 * 10) {
+            currenRecord[6] = currenRecord[6] + 1
+        }
 
-
-
-variable.last20Buy = []
-variable.last20Sell = []
-variable.last200Buy = []
-variable.last200Sell = []
-
-for (let i = 0; i < 20; i++) {
-    variable.last20Buy.push(0)
-    variable.last20Sell.push(0)
+        currenRecord[7] = currenRecord[7] + 1
+    }
 }
 
-for (let i = 0; i < 200; i++) {
-    variable.last200Buy.push(0)
-    variable.last200Sell.push(0)
+function calculateSupportResistanceZone() {
+    /* 
+    The variable % is a percentage we will use for support and resistance based on the time frame.
+    We will fix the % for 1 minute candles and for 24 hs candle and the use the formula to obtain
+    y on a line by knowing 2 points of the line as described here: https://en.wikipedia.org/wiki/Line_(geometry)
+    */
+
+    let x0 = system.ONE_MIN_IN_MILISECONDS
+    let y0 = 0.1 // At one min, we will consider the resistance / support zone to be 0.1% resistance 
+    let x1 = system.ONE_DAY_IN_MILISECONDS
+    let y1 = 4 // At one day, we will consider the resistance / support zone to be 4% resistance
+
+    let x = system.timeFrame  // This is the current time frame being calculated
+    let y = (x - x0) * (y1 - y0) / (x1 - x0) + y0
+
+    return y
+
 }
-
-
-
-

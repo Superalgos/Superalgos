9e00cd2468b5f826f87ea5eb327dcf62a1b1278d
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-11-20 19:46:12 +0100

    Operations Module moved to old repos

--- server/src/config/utils.js
+++
@@ -1,9 +0,0 @@
-export function toPlatformDatetime (epoch) {
-  const platformDate = new Date(0)
-  platformDate.setUTCSeconds(epoch)
-  return platformDate.toISOString()
-}
-
-export function isDefined (d) {
-  return d !== null && typeof d !== 'undefined'
-}

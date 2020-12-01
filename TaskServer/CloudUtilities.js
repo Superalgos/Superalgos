503a7d9283ab0438db5fb0547ee67dbc5334310b
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-12-01 16:08:53 +0100

    Code not used anymore

--- TaskServer/CloudUtilities.js
+++ TaskServer/CloudUtilities.js
@@ -5,55 +5,10 @@ exports.newCloudUtilities = function newCloudUtilities() {
 
     let utilities = {
         pad: pad,
-        createFolderIfNeeded: createFolderIfNeeded,
         calculatePresicion: calculatePresicion
     };
 
     return utilities;
-
-    function createFolderIfNeeded(path, azureFileStorage, callBackFunction) {
-
-        try {
-            TS.projects.superalgos.globals.loggerVariables.VARIABLES_BY_PROCESS_INDEX_MAP.get(processIndex).BOT_MAIN_LOOP_LOGGER_MODULE.write(MODULE_NAME, "[INFO] createFolderIfNeeded -> path = " + path)
-
-            let splittedPath = path.split("/");
-
-            let partialPath = '';
-            let separator = "";
-
-            let i = 0;
-            loop();
-
-            function loop() {
-                TS.projects.superalgos.globals.loggerVariables.VARIABLES_BY_PROCESS_INDEX_MAP.get(processIndex).BOT_MAIN_LOOP_LOGGER_MODULE.write(MODULE_NAME, "[INFO] createFolderIfNeeded -> path = " + path)
-
-                partialPath = partialPath + separator + splittedPath[i];
-                separator = "/";
-                i++;
-                TS.projects.superalgos.globals.loggerVariables.VARIABLES_BY_PROCESS_INDEX_MAP.get(processIndex).BOT_MAIN_LOOP_LOGGER_MODULE.write(MODULE_NAME, "[INFO] createFolderIfNeeded -> partialPath = " + partialPath)
-
-                azureFileStorage.createFolder(partialPath, checkLoop);
-
-                function checkLoop(err) {
-                    if (err.result !== TS.projects.superalgos.globals.standardResponses.DEFAULT_OK_RESPONSE.result) {
-                        TS.projects.superalgos.globals.loggerVariables.VARIABLES_BY_PROCESS_INDEX_MAP.get(processIndex).BOT_MAIN_LOOP_LOGGER_MODULE.write(MODULE_NAME, "[ERROR] createFolderIfNeeded -> loop -> checkLoop -> err = " + err.stack);
-                        callBackFunction(err);
-                        return;
-                    }
-
-                    if (i === splittedPath.length) {
-                        callBackFunction(TS.projects.superalgos.globals.standardResponses.DEFAULT_OK_RESPONSE);
-                    } else {
-                        loop();
-                    }
-                }
-            }
-        } catch (err) {
-            TS.projects.superalgos.globals.loggerVariables.VARIABLES_BY_PROCESS_INDEX_MAP.get(processIndex).BOT_MAIN_LOOP_LOGGER_MODULE.write(MODULE_NAME, "[ERROR] createFolderIfNeeded -> Error = " + err.message);
-            callBackFunction(TS.projects.superalgos.globals.standardResponses.DEFAULT_FAIL_RESPONSE);
-        }
-    }
-
 
     function calculatePresicion(number) {
 

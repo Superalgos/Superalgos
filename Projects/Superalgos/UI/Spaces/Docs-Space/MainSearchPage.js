3f2bd1c673a8bdf4e92fc9b34c5be02e8b5797f3
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2020-12-31 09:59:06 +0100

    Ready to reorganize into different files

---
+++ Projects/Superalgos/UI/Spaces/Docs-Space/MainSearchPage.js
@@ -0,0 +1,16 @@
+function newSuperalgosDocsMainSearchPage() {
+    let thisObject = {
+        initialize: initialize,
+        finalize: finalize
+    }
+
+    return thisObject
+
+    function initialize() {
+
+    }
+
+    function finalize() {
+
+    }
+}
---
+++ Projects/Superalgos/UI/Spaces/Docs-Space/SearchEngine.js
@@ -0,0 +1,16 @@
+function newSuperalgosDocsSearchEngine() {
+    let thisObject = {
+        initialize: initialize,
+        finalize: finalize
+    }
+
+    return thisObject
+
+    function initialize() {
+
+    }
+
+    function finalize() {
+
+    }
+}

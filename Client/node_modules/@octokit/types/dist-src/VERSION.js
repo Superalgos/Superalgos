a9012f1bbf07b805776631a040a92ce0f80fe4c6
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-01-06 10:55:01 +0100

    Github API

---
+++ Client/node_modules/@octokit/types/dist-src/VERSION.js
@@ -0,0 +1,1 @@
+export const VERSION = "6.2.1";
--- Client/test.js
+++ Client/test.js
@@ -1,7 +1,64 @@
+githubTest()
+//gitTest()
 
-doIt()
 
-async function doIt() {
+async function githubTest() {
+
+    const token = ""
+    const { Octokit } = require("@octokit/rest")
+
+    const octokit = new Octokit({
+        auth: token,
+        userAgent: 'Superalgos Beta 8',
+        //previews: ['jean-grey', 'symmetra'],
+        //timeZone: 'Europe/London',
+        //baseUrl: 'https://api.github.com',
+        /*
+        log: {
+            debug: () => { },
+            info: () => { },
+            warn: console.warn,
+            error: console.error
+        },
+        request: {
+            agent: undefined,
+            fetch: undefined,
+            timeout: 0
+        }
+        */
+    })
+
+    const repo = 'Superalgos'
+    const owner = 'Luis-Fernando-Molina'
+    const username = 'Luis-Fernando-Molina'
+    const head = 'Luis-Fernando-Molina:in-app-documentation'
+    const base = 'in-app-documentation'
+
+    /*
+    let user = await octokit.users.getByUsername({
+        username,
+    });
+
+    let superalgosRepo = await octokit.repos.get({
+        owner,
+        repo,
+      });
+    */
+
+    let pullRequest = await octokit.pulls.create({
+        owner,
+        repo,
+        head,
+        base,
+    });
+
+    console.log(pullRequest)
+}
+
+
+
+
+async function gitTest() {
     const simpleGit = require('simple-git');
     const options = {
         baseDir: process.cwd(),
@@ -12,7 +69,7 @@ async function doIt() {
 
     let remotes = await git.getRemotes(true)
 
-    
+
 
     console.log('JSON remotes list: ', JSON.stringify(remotes))
     console.log('remotes list: ', remotes)

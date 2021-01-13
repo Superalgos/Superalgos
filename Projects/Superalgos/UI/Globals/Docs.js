61eb09df8ce200124bddb8b18d6d713b8e1755dc
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-01-13 17:49:43 +0100

    Default branch to develop

--- Projects/Superalgos/UI/Globals/Docs.js
+++ Projects/Superalgos/UI/Globals/Docs.js
@@ -2,7 +2,7 @@ function newSuperalgosGlobalsDocs() {
     thisObject = {
         NEW_PARAGRAPH_TEXT: "Left click and Edit to enter edit mode and change this text. ENTER to write new paragraphs. ESC to exit edit mode.",
         DEFAULT_LANGUAGE: 'EN',
-        DEFAULT_ACTIVE_BRANCH: 'master',
+        DEFAULT_CURRENT_BRANCH: 'develop',
         DEFAULT_CONTRIBUTIONS_BRANCH: 'develop',
         DOCS_SPACE_WIDTH: 900
     }

ae38c94bd731b1c588c4b88fe4edcd5afe30bd18
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-03-29 15:24:19 +0200

    closeTutorialEditor command improved to show search page

--- Projects/Superalgos/UI/Spaces/Tutorial-Space/TutorialSpace.js
+++ Projects/Superalgos/UI/Spaces/Tutorial-Space/TutorialSpace.js
@@ -1200,6 +1200,8 @@ function newSuperalgosTutorialSpace() {
         if (nodeConfig.controlDocs !== undefined) {
             if (nodeConfig.controlDocs.closeTutorialEditor !== true) {
                 UI.projects.superalgos.spaces.docsSpace.navigateTo(nodeConfig.docs.project, 'Tutorial', nodeConfig.docs.type)
+            } else {
+                UI.projects.superalgos.spaces.docsSpace.searchPage()
             }
         } else {
             UI.projects.superalgos.spaces.docsSpace.navigateTo(nodeConfig.docs.project, 'Tutorial', nodeConfig.docs.type)

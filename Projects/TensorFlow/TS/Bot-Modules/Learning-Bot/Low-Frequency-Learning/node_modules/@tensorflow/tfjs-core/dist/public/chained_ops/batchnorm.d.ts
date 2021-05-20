ac5774dfb27f760b369db93e39d528d389d7d16a
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-05-20 21:39:08 +0200

    node_modules folders removed from all places

--- Projects/TensorFlow/TS/Bot-Modules/Learning-Bot/Low-Frequency-Learning/node_modules/@tensorflow/tfjs-core/dist/public/chained_ops/batchnorm.d.ts
+++
@@ -1,6 +0,0 @@
-import { Rank, TensorLike } from '../../types';
-declare module '../../tensor' {
-    interface Tensor<R extends Rank = Rank> {
-        batchNorm<T extends Tensor>(mean: Tensor<R> | Tensor1D | TensorLike, variance: Tensor<R> | Tensor1D | TensorLike, offset?: Tensor<R> | Tensor1D | TensorLike, scale?: Tensor<R> | Tensor1D | TensorLike, varianceEpsilon?: number): Tensor<R>;
-    }
-}
--- Projects/TensorFlow/TS/Bot-Modules/Learning-Bot/Low-Frequency-Learning/node_modules/@tensorflow/tfjs-core/dist/public/chained_ops/minimum.d.ts
+++
@@ -1,6 +0,0 @@
-import { Rank, TensorLike } from '../../types';
-declare module '../../tensor' {
-    interface Tensor<R extends Rank = Rank> {
-        minimum<T extends Tensor>(b: Tensor | TensorLike): T;
-    }
-}

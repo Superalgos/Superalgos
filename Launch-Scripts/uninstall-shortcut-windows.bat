b1bd4a350ab478a7bea2bc093c02d939af51548b
Brenden Harrell <brendenharrell1@gmail.com>
2021-05-23 15:21:50 -0500

    Delete: Old Uninstall Script

--- Launch-Scripts/uninstall-shortcut-windows.bat
+++
@@ -1,5 +0,0 @@
-del "%USERPROFILE%\Desktop\Superalgos.lnk"
-del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Superalgos.lnk"
-
-REM Keep terminal open for debug
-REM cmd /k echo "keep open"

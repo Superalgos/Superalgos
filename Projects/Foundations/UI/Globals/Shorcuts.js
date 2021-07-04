827dcbaee6a04970ba7839bdc80064b37d6b2d27
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-07-04 19:01:58 +0200

    Floating Space Settings

--- Projects/Foundations/UI/Globals/Shorcuts.js
+++ Projects/Foundations/UI/Globals/Shorcuts.js
@@ -3,7 +3,8 @@ function newFoundationsGlobalsShorcuts() {
         TOGGLE_MAP_MODE: 'M',
         TOGGLE_DRAW_REFERENCE_LINES: 'R',
         TOGGLE_DRAW_PARENT_LINES: 'C',
-        SAVE_WORKSPACE: 'S'
+        SAVE_WORKSPACE: 'S',
+        ADJUST_ASPECT_RATIO: 'A'
     }
     return thisObject
 }

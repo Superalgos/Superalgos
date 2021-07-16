072cca8e6b6942e28e0b18eabe3220ecf56026a3
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-07-16 18:58:54 +0200

    Filters at Utilities

--- Projects/Governance/UI/Spaces/Reports-Space/UserProfiles.js
+++ Projects/Governance/UI/Spaces/Reports-Space/UserProfiles.js
@@ -83,34 +83,7 @@ function newGovernanceReportsUserProfiles() {
                 "tokenPower": userProfile.payload.tokenPower | 0
             }
 
-            /* Apply Filters */
-            let passedFilters = true
-            if (filtersObject !== undefined) {
-                for (var property in tableRecord) {
-                    if (Object.prototype.hasOwnProperty.call(tableRecord, property)) {
-                        let fiter = filtersObject[property]
-                        if (fiter === undefined) { continue }
-                        let value = tableRecord[property]
-                        let condition = eval(fiter)
-                        if (condition === false) {
-                            passedFilters = false
-                        }
-                    }
-                }
-            } else {
-                /*
-                We will assume the filter is a string and that needs to be applied to the property name.
-                */
-                if (
-                    filters !== undefined &&
-                    filters !== "" &&
-                    tableRecord.name.indexOf(filters) < 0
-                ) {
-                    passedFilters = false
-                }
-            }
-
-            if (passedFilters === true) {
+            if (UI.projects.governance.utilities.filters.applyFilters(filters, filtersObject, tableRecord) === true) {
                 tableRecords.push(tableRecord)
             }
         }

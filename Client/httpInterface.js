0fc35f0dc224bd8be3e24ef498d8b2d0d51b8734
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-08-08 15:31:08 +0200

    Governance: Validation #5 test part 2

--- Client/httpInterface.js
+++ Client/httpInterface.js
@@ -2062,38 +2062,4 @@ exports.newHttpInterface = function newHttpInterface(
             return files.reduce((a, f) => a.concat(f), []);
         }
     }
-
-    async function fetchAPIDataFile(url) {
-        let promise = new Promise((resolve, reject) => {
-
-            const fetch = require('node-fetch')
-            /*
-            This is how we call the API.
-            */
-            fetch(url)
-                .then((response) => {
-
-                    if (response.status !== 200) {
-                        console.log('[ERROR] fetchAPIData -> then -> url =' + url)
-                        reject()
-                        return
-                    }
-                    getResponseBody()
-
-                    function getResponseBody() {
-                        response.text().then(body => {
-                            resolve(body)
-                        })
-                    }
-                })
-                .catch(err => {
-                    console.log('[ERROR] fetchAPIData -> url =' + url)
-                    reject()
-                })
-
-        }
-        )
-
-        return promise
-    }
 }

f381eafe9da9a151559fc51514ff275cbdfc2ea1
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-08-24 12:57:28 +0200

    DeepScan bug fixes

--- Client/webSocketsInterface.js
+++ Client/webSocketsInterface.js
@@ -1,6 +1,5 @@
 ﻿exports.newWebSocketsInterface = function newWebSocketsInterface() {
 
-    const MODULE = "Web Sockets Interface"
     const LOG_INFO = false
 
     let thisObject = {
@@ -76,7 +75,7 @@
                             lastNonce = nonce
 
                             try {
-                                let jsonCheck = JSON.parse(messageToEventServer)
+                                JSON.parse(messageToEventServer)
                             } catch (err) {
                                 console.log('[ERROR] Client -> Web Sockets Interface -> run -> setUpWebSocketServer -> Message received from the browser is not a valid JSON. message = ' + message.substring(0, 1000))
                                 console.log('[ERROR] Client -> Web Sockets Interface -> run -> setUpWebSocketServer -> Message received from the browser is not a valid JSON. messageToEventServer = ' + messageToEventServer)

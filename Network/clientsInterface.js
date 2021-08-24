cc47c888c751223538152262b7996f36fc45fd52
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-08-24 12:06:39 +0200

    Drafting the Superalgos Network

---
+++ Network/clientsInterface.js
@@ -0,0 +1,22 @@
+exports.newClientInterface = function newClientInterface() {
+
+    let thisObject = {
+        initialize: initialize,
+        finalize: finalize,
+        run: run
+    }
+
+    return thisObject
+
+    function finalize() {
+
+    }
+
+    function initialize() {
+
+    }
+
+    function run() {
+        
+     }
+}
---
+++ Network/node.js
@@ -0,0 +1,7 @@
+
+
+let usersProfiles = new Map()       // Here we will store the user profiles and their statistics.
+let timeline = []                   // A time ordered list of Posts and Trades.
+let events = new Map()
+let hashGraph = {}
+

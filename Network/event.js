979670a7aab034a4db04bdc65bad6a97ab4ffae6
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-08-25 16:18:41 +0200

    Follow / Unfollow event types

--- Network/event.js
+++ Network/event.js
@@ -16,14 +16,14 @@ exports.newEvent = function newEvent() {
         REPLY_TO_MULTI_MEDIA_POST: 11,
         REPOST_MULTI_MEDIA: 12,
         QUOTE_REPOST_MULTI_MEDIA: 13,
-        FOLLOW_MULTI_MEDIA_POSTS: 14,
-        UNFOLLOW_MULTIMEDIA_POSTS: 15,
+        FOLLOW_USER_PROFILE_MULTI_MEDIA_POSTS: 14,
+        UNFOLLOW_USER_PROFILE_MULTI_MEDIA_POSTS: 15,
         NEW_TRADE_POST: 20,
         REPLY_TO_TRADE_POST: 21,
         RE_POST_TRADE: 22,
         QUOTE_REPOST_TRADE: 23,
-        FOLLOW_TRADE_POSTS: 24,
-        UNFOLLOW_TRADE_POSTS: 25,
+        FOLLOW_USER_PROFILE_TRADE_POSTS: 24,
+        UNFOLLOW_USER_PROFILE_TRADE_POSTS: 25,
         REACTION_LIKE: 100,
         REACTION_LOVE: 101,
         REACTION_HAHA: 102,
@@ -66,12 +66,12 @@ exports.newEvent = function newEvent() {
 
         thisObject.targetUserProfile = NT.memory.USER_PROFILES.get(event.targetUserProfileId)
         if (thisObject.targetUserProfile === undefined) {
-            /* The message is ignored when it does not have a target user profile and is required*/
+            /* We thow an exception when it does not have a target user profile and is required*/
             if (
                 event.eventType !== EVENT_TYPES.NEW_MULTI_MEDIA_POST &&
                 event.eventType !== EVENT_TYPES.NEW_TRADE_POST
             )
-                return
+                throw ('Target User Profile Not Found.')
         }
 
         thisObject.eventId = event.eventId
@@ -79,28 +79,78 @@ exports.newEvent = function newEvent() {
         thisObject.asset = event.asset
         thisObject.timestamp = event.timestamp
 
-        switch (true) {
-            case (
-                event.eventType === EVENT_TYPES.NEW_MULTI_MEDIA_POST ||
-                event.eventType === EVENT_TYPES.REPLY_TO_MULTI_MEDIA_POST ||
-                event.eventType === EVENT_TYPES.REPOST_MULTI_MEDIA ||
-                event.eventType === EVENT_TYPES.QUOTE_REPOST_MULTI_MEDIA ||
-                event.eventType === EVENT_TYPES.NEW_TRADE_POST ||
-                event.eventType === EVENT_TYPES.REPLY_TO_TRADE_POST ||
-                event.eventType === EVENT_TYPES.RE_POST_TRADE ||
-                event.eventType === EVENT_TYPES.QUOTE_REPOST_TRADE
-            ): {
-                    let post = NT.modules.POST.newPost()
-                    post.initialize(
-                        event.emitterPostHash,
-                        event.targetPostHash,
-                        thisObject.eventType,
-                        thisObject.emitterUserProfile,
-                        thisObject.asset,
-                        thisObject.timestamp
+        /*
+        Is is a new post?
+        */
+        if (
+            event.eventType === EVENT_TYPES.NEW_MULTI_MEDIA_POST ||
+            event.eventType === EVENT_TYPES.REPLY_TO_MULTI_MEDIA_POST ||
+            event.eventType === EVENT_TYPES.REPOST_MULTI_MEDIA ||
+            event.eventType === EVENT_TYPES.QUOTE_REPOST_MULTI_MEDIA ||
+            event.eventType === EVENT_TYPES.NEW_TRADE_POST ||
+            event.eventType === EVENT_TYPES.REPLY_TO_TRADE_POST ||
+            event.eventType === EVENT_TYPES.RE_POST_TRADE ||
+            event.eventType === EVENT_TYPES.QUOTE_REPOST_TRADE
+        ) {
+            let post = NT.modules.POST.newPost()
+            post.initialize(
+                event.emitterPostHash,
+                event.targetPostHash,
+                thisObject.eventType,
+                thisObject.emitterUserProfile,
+                thisObject.asset,
+                thisObject.timestamp
+            )
+            return
+        }
+        /*
+        Is is a following?
+        */
+        if (
+            event.eventType === EVENT_TYPES.FOLLOW_USER_PROFILE_MULTI_MEDIA_POSTS ||
+            event.eventType === EVENT_TYPES.UNFOLLOW_USER_PROFILE_MULTI_MEDIA_POSTS ||
+            event.eventType === EVENT_TYPES.FOLLOW_USER_PROFILE_TRADE_POSTS ||
+            event.eventType === EVENT_TYPES.UNFOLLOW_USER_PROFILE_TRADE_POSTS
+        ) {
+            switch (event.eventType) {
+                case EVENT_TYPES.FOLLOW_USER_PROFILE_MULTI_MEDIA_POSTS: {
+                    thisObject.emitterUserProfile.addMultiMediaPostsFollowing(
+                        thisObject.targetUserProfile
+                    )
+                    thisObject.targetUserProfile.addMultiMediaPostsFollower(
+                        thisObject.emitterUserProfile
                     )
                     break
                 }
+                case EVENT_TYPES.UNFOLLOW_USER_PROFILE_MULTI_MEDIA_POSTS: {
+                    thisObject.emitterUserProfile.removeMultiMediaPostsFollowing(
+                        thisObject.targetUserProfile
+                    )
+                    thisObject.targetUserProfile.removeMultiMediaPostsFollower(
+                        thisObject.emitterUserProfile
+                    )
+                    break
+                }
+                case EVENT_TYPES.FOLLOW_USER_PROFILE_TRADE_POSTS: {
+                    thisObject.emitterUserProfile.addTradePostsFollowing(
+                        thisObject.targetUserProfile
+                    )
+                    thisObject.targetUserProfile.addTradePostsFollower(
+                        thisObject.emitterUserProfile
+                    )
+                    break
+                }
+                case EVENT_TYPES.UNFOLLOW_USER_PROFILE_TRADE_POSTS: {
+                    thisObject.emitterUserProfile.removeTradePostsFollowing(
+                        thisObject.targetUserProfile
+                    )
+                    thisObject.targetUserProfile.removeTradePostsFollower(
+                        thisObject.emitterUserProfile
+                    )
+                    break
+                }
+            }
+            return
         }
     }
 }

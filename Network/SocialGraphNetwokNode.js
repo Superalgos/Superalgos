06b5b9956c44cbfe838ae94425d0c7061804f7a9
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-08-29 17:45:53 +0200

    Cleaning

--- Network/SocialGraphNetwokNode.js
+++ Network/SocialGraphNetwokNode.js
@@ -5,7 +5,6 @@ global.NT = {
         maps: {
             USER_PROFILES_BY_ID: new Map(),         // Here we will store the user profiles by User Profile Id.
             USER_PROFILES_BY_HANDLE: new Map(),     // Here we will store the user profiles by User Profile Handle.
-            POSTS: new Map(),                       // This are all the posts we are keeping track of.
             EVENTS: new Map(),                      // This is the registry of all events received that prevents processing them more than once.
         },
         arrays: {
@@ -23,8 +22,8 @@ global.NT = {
         QUERY_BOT_PROFILE_STATS: require('./Queries/BotProfileStats.js'),
         QUERY_PROFILE_POSTS: require('./Queries/ProfilePosts.js'),
         QUERY_PROFILE_FOLLOWERS: require('./Queries/ProfileFollowers.js'),
-        //QUERY_PROFILE_FOLLOWING: require('./Queries/ProfileFollowing.js'),
-        //QUERY_POST_REPLIES: require('./Queries/ProfileReplies.js'),
+        QUERY_PROFILE_FOLLOWING: require('./Queries/ProfileFollowing.js'),
+        QUERY_POST_REPLIES: require('./Queries/PostReplies.js'),
         QUERY_EVENTS: require('./Queries/Events.js')
     },
     utilities: {
@@ -44,10 +43,3 @@ global.NT = {
     }
 }
 
-const map = new Map([[1, { a: 2 }], [2, { a: 4 }], [4, { a: 8 }]])
-map.set(3, { a: 6 })
-map.delete(2)
-map.set(2, { a: 12 })
-let array = Array.from(map)
-
-console.log(JSON.stringify(array))

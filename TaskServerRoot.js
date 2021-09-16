f990734a875140f765be3b3980df570f28055bd4
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-09-16 10:31:18 +0200

    Task Server Root implemented

---
+++ TaskServerRoot.js
@@ -0,0 +1,51 @@
+
+/*
+This module represents the execution root of the Task Server.
+We use this module that is outside the Task Server folder to 
+load all node dependencies and get them ready to the actual App.
+*/
+
+/* 
+The TS object is accesible everywhere at the Superalgos Client. 
+It provides access to all modules built for the Task Server.
+*/
+global.TS = {}
+/* 
+The SA object is accesible everywhere at the Superalgos Desktop App. 
+It provides access to all modules built for Superalgos in general.
+*/
+global.SA = {}
+/* Load Environment Variables */
+let ENVIRONMENT = require('./Environment.js');
+let ENVIRONMENT_MODULE = ENVIRONMENT.newEnvironment()
+global.env = ENVIRONMENT_MODULE
+/*
+First thing is to load the project schema file.
+*/
+global.PROJECTS_SCHEMA = require(global.env.PATH_TO_PROJECT_SCHEMA)
+/* 
+Setting up the modules that will be available, defined at the Project Schema file. 
+*/
+let MULTI_PROJECT = require('./MultiProject.js');
+let MULTI_PROJECT_MODULE = MULTI_PROJECT.newMultiProject()
+MULTI_PROJECT_MODULE.initialize(TS, 'TS')
+MULTI_PROJECT_MODULE.initialize(SA, 'SA')
+/*
+Setting up external dependencies.
+*/
+SA.nodeModules = {
+    fs: require('fs'),
+    web3: require('web3'),
+    nodeFetch: require('node-fetch'),
+    http: require('http'),
+    ccxt: require('ccxt'),
+    lookpath: require('lookpath')
+}
+
+run()
+
+async function run() {
+    TS.app = require('./TaskServer/TaskServer.js').newTaskServer()
+    await TS.app.run()
+    console.log('Superalgos TaskSerer is Running!')
+}

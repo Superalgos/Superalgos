9f3c91cc54bd9818fa203027048e0f4540991114
mARTin-B78 <mail@martin-bierschenk.de>
2021-10-16 12:29:02 +0000

    This is my contribution to Superalgos

--- Projects/Education/UI/Spaces/Tutorial-Space/TutorialSpace.js
+++ Projects/Education/UI/Spaces/Tutorial-Space/TutorialSpace.js
@@ -1380,11 +1380,11 @@ function newEducationTutorialSpace() {
                 }
             }
 
+            html = html + '<div class="tutorial-content-div">'
+
             if (fullscreenMode === false) {
                 html = html + '<h2 class="tutorial-font-medium">' + title + '</h2>'
             }
-
-            html = html + '<div class="tutorial-content-div">'
 
             if (schemaDocument.definition !== undefined && schemaDocument.definition.text !== '') {
                 if (fullscreenMode === false) {

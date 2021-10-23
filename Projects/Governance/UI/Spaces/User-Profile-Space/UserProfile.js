80920eabb36a90520e1b2763d9e07aeb700f97d4
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-10-23 14:49:22 +0200

    liquidity Balances Laoding Well

--- Projects/Governance/UI/Spaces/User-Profile-Space/UserProfile.js
+++ Projects/Governance/UI/Spaces/User-Profile-Space/UserProfile.js
@@ -15,7 +15,7 @@ function newGovernanceUserProfileSpace() {
     }
 
     let waitingForResponses = 0
-    const BSC_SCAN_RATE_LIMIT_DELAY = 6000 * 4
+    const BSC_SCAN_RATE_LIMIT_DELAY = 6000 * 5
     let reputationByAddress = new Map()
 
     return thisObject
@@ -373,7 +373,12 @@ function newGovernanceUserProfileSpace() {
             let userProfile = userProfiles[i]
             if (userProfile.payload === undefined) { continue }
 
+            if (userProfile.payload.bloackchainBalancesLoading === true) {
+                return
+            }
+
             if (userProfile.payload.blockchainTokens === undefined) {
+                userProfile.payload.bloackchainBalancesLoading = true           
                 getBlockchainAccount(userProfile)
                 return
             }
@@ -394,6 +399,7 @@ function newGovernanceUserProfileSpace() {
             httpRequest(JSON.stringify(request.params), request.url, onResponse)
 
             function onResponse(err, data) {
+
                 /* Lets check the result of the call through the http interface */
                 if (err.result !== GLOBAL.DEFAULT_OK_RESPONSE.result) {
                     userProfile.payload.uiObject.setErrorMessage(
@@ -422,13 +428,16 @@ function newGovernanceUserProfileSpace() {
                     blockchainAccount !== "" &&
                     userProfile.payload.blockchainTokens === undefined
                 ) {
-                    waitingForResponses++
+                                                 
                     userProfile.payload.liquidityTokens = {
                         BTCB: 0,
                         BNB: 0,
                         BUSD: 0
                     }
 
+                    waitingForResponses++
+                    waitingForResponses++
+                    waitingForResponses++
                     setTimeout(getBPancakeTokens, BSC_SCAN_RATE_LIMIT_DELAY / 4 * 1, userProfile, blockchainAccount, 'BTCB', UI.projects.governance.globals.saToken.SA_TOKEN_BSC_PANCAKE_LIQUIDITY_POOL_BTCB_CONTRACT_ADDRESS)
                     setTimeout(getBPancakeTokens, BSC_SCAN_RATE_LIMIT_DELAY / 4 * 2, userProfile, blockchainAccount, 'BNB', UI.projects.governance.globals.saToken.SA_TOKEN_BSC_PANCAKE_LIQUIDITY_POOL_BNB_CONTRACT_ADDRESS)
                     setTimeout(getBPancakeTokens, BSC_SCAN_RATE_LIMIT_DELAY / 4 * 3, userProfile, blockchainAccount, 'BUSD', UI.projects.governance.globals.saToken.SA_TOKEN_BSC_PANCAKE_LIQUIDITY_POOL_BUSD_CONTRACT_ADDRESS)
@@ -437,7 +446,6 @@ function newGovernanceUserProfileSpace() {
                     Now we get the SA Tokens Balance.
                     */
                     waitingForResponses++
-                    userProfile.payload.blockchainTokens = 0 // We need to set this value here so that the next call to BSCSCAN is not done more than once.
                     setTimeout(getBlockchainTokens, BSC_SCAN_RATE_LIMIT_DELAY / 4 * 4, userProfile, blockchainAccount)
                 }
             }
@@ -450,14 +458,15 @@ function newGovernanceUserProfileSpace() {
             fetch(url).then(function (response) {
                 return response.json();
             }).then(function (data) {
-                //console.log(data)
+                userProfile.payload.bloackchainBalancesLoading = false
                 if (data.result === "Max rate limit reached, please use API Key for higher rate limit") {
-                    userProfile.payload.blockchainTokens = undefined // This enables this profile to query the blockchain again.
+                    userProfile.payload.blockchainTokens = undefined 
                 } else {
                     userProfile.payload.uiObject.setInfoMessage('Blockchan Balance Succesfully Loaded.',
                         UI.projects.governance.globals.designer.SET_INFO_COUNTER_FACTOR
                     )
                     userProfile.payload.blockchainTokens = Number(data.result) / 1000000000000000000
+                    console.log('[INFO] SA Balance of ' + userProfile.name + ' is ', userProfile.payload.blockchainTokens)
                     userProfile.payload.reputation = Math.min(reputationByAddress.get(blockchainAccount.toLowerCase()) | 0, userProfile.payload.blockchainTokens)
                     console.log('[INFO] Reputation of ' + userProfile.name + ' is ', userProfile.payload.reputation)
                 }
@@ -475,7 +484,7 @@ function newGovernanceUserProfileSpace() {
         }
 
         function getBPancakeTokens(userProfile, blockchainAccount, asset, marketContract) {
-            console.log('[INFO] Loading Pancake Balance for User Profile: ', userProfile.name, 'blockchainAccount: ', blockchainAccount)
+            console.log('[INFO] Loading Pancake Balance for User Profile: ', userProfile.name, 'blockchainAccount: ', blockchainAccount, 'asset: ', asset)
             const url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=" + marketContract + "&address=" + blockchainAccount + "&tag=latest&apikey=YourApiKeyToken"
 
             fetch(url).then(function (response) {
@@ -483,9 +492,10 @@ function newGovernanceUserProfileSpace() {
             }).then(function (data) {
                 //console.log(data)
                 if (data.result === "Max rate limit reached, please use API Key for higher rate limit") {
-                    userProfile.payload.liquidityTokens = undefined // This enables this profile to query the blockchain again.
+                    console.log('[WARN] Rate Limit Reached fetching liquidity tokens for asset ' + asset + ' of user profile ' + userProfile.name)
+                    userProfile.payload.blockchainTokens = undefined 
                 } else {
-                    userProfile.payload.uiObject.setInfoMessage('Pancake Balance Succesfully Loaded.',
+                    userProfile.payload.uiObject.setInfoMessage('Pancake Balance Succesfully Loaded for asset ' + asset,
                         UI.projects.governance.globals.designer.SET_INFO_COUNTER_FACTOR
                     )
                     userProfile.payload.liquidityTokens[asset] = Number(data.result) / 1000000000000000000

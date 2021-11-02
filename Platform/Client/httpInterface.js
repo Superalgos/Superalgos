d10fbc656f7e146a9e13d44e8405767091055f97
NAmorim <na.amorim@gmail.com>
2021-11-02 00:21:37 +0000

    Change the console message

--- Platform/Client/httpInterface.js
+++ Platform/Client/httpInterface.js
@@ -642,7 +642,7 @@ exports.newHttpInterface = function newHttpInterface() {
                     {
                         // If running the electron app do not try to get git tool. I don't allow it.
                         if (process.env.SA_MODE === 'gitDisable') {
-                            console.log('[WARNING] No contributions on binary distributions. Do manual installation')
+                            console.log('[WARN] No contributions on binary distributions. Do manual installation')
                             break
                         }
                         switch (requestPath[2]) { // switch by command

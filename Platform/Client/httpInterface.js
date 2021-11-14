9cf48760077977f7b8b91b09f44a0d54d2fa04a6
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2021-11-14 15:03:55 +0100

    v.1.0 replaces Beta 12

--- Platform/Client/httpInterface.js
+++ Platform/Client/httpInterface.js
@@ -772,7 +772,7 @@ exports.newHttpInterface = function newHttpInterface() {
 
                                     const octokit = new Octokit({
                                         auth: token,
-                                        userAgent: 'Superalgos Beta 12'
+                                        userAgent: 'Superalgos v.1.0'
                                     })
 
                                     const repo = 'Superalgos'
@@ -1347,7 +1347,7 @@ exports.newHttpInterface = function newHttpInterface() {
 
                                             const octokit = new Octokit({
                                                 auth: token,
-                                                userAgent: 'Superalgos Beta 12'
+                                                userAgent: 'Superalgos v.1.0'
                                             })
 
                                             const repo = 'Superalgos'
@@ -1861,7 +1861,7 @@ exports.newHttpInterface = function newHttpInterface() {
                             let folder = unescape(requestPath[3])
                             let fileName = unescape(requestPath[4])
 
-                            /*Beta 12 Refactoring Code: Remove this before releasing beta 12.*/
+                            /*v.1.0 Refactoring Code: Remove this before releasing v.1.0.*/
                             if (fileName === 'Superalgos-CL.json') {
                                 fileName = 'Superalgos-PL.json'
                             }

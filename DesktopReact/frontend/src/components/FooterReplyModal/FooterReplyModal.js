d5b84570bb0d254e04c320ebc899180e640bcfcc
Claudio <claudiosuperalgos@gmail.com>
2021-12-23 19:54:04 -0300

    [Desktop] Added functionality

--- DesktopReact/frontend/src/components/FooterReplyModal/FooterReplyModal.js
+++ DesktopReact/frontend/src/components/FooterReplyModal/FooterReplyModal.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Box, Typography} from "@mui/material";
+import React, {useEffect, useState} from 'react';
+import {Box, Modal, Typography} from "@mui/material";
+import {getPosts} from "../../api/post.httpService";
 
 /* TODO Mode style to css if posible */
 const style = {
@@ -7,24 +8,45 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: '40rem',
+    height: '25.2rem',
     bgcolor: 'background.paper',
-    border: '2px solid #000',
+    border: '1px solid #000',
     boxShadow: 24,
-    p: 4,
 };
 
-const body = "Duis mollis, est non commodo luctus, nisi erat porttitor ligula."
-const FooterReplyModal = () => {
+const FooterReplyModal = ({show, close}) => {
+    const [modalBody, setModalBody] = useState()
+    const modalBodyValue = () => {
+        let data = getPosts()
+        setModalBody(data)
+    }
+
+    /*useEffect(() => {
+        return modalBodyValue();
+    }, []); fix needed, breaks the code */
+
     return (
-        <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-                Text in a modal
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                 {body}
-            </Typography>
-        </Box>
+        <>
+            {show ?
+                <Modal open={show} /* todo unfinished, need style */
+                       onClose={close}
+                >
+                    <Box sx={style}>
+                        <div>
+                            <Typography>
+                                Post to reply to
+                            </Typography>
+                            {modalBody}
+                        </div>
+                        <div>
+                            <Typography>
+                                Reply Component
+                            </Typography>
+                        </div>
+                    </Box>
+                </Modal> : null}
+        </>
     );
 };
 

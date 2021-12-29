c6ba32ec77a7ad404434c45ec142fd3e293023fa
Claudio <claudiosuperalgos@gmail.com>
2021-12-28 22:01:37 -0300

    [Desktop] Removed unused style Added state & handlers to inputBoxes Added cardMedia to header Added buttons for edit

--- DesktopReact/frontend/src/components/userProfileHeader/UserProfileModal.js
+++ DesktopReact/frontend/src/components/userProfileHeader/UserProfileModal.js
@@ -1,55 +1,91 @@
 import React, {useState} from 'react';
-import {Box, Button, CardContent, Modal, TextField, Typography} from "@mui/material";
+import {Box, Button, CardContent, CardMedia, Modal, TextField, Typography} from "@mui/material";
 import "./UserProfileModal.css"
 import {CloseOutlined} from "@mui/icons-material";
-
-const style = { // todo need proper style, and handle from css file
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '40rem',
-    height: '25.2rem',
-    bgcolor: 'background.paper',
-    border: '1px solid #000',
-    boxShadow: 24,
-};
+import banner from "../../images/banner.jpg";
+import pfp from "../../images/superalgos.png";
 
 const UserProfileModal = ({show, close}) => {
 
     const [inputValue, setInputValue] = useState()
-    const handleInput = () => {
-        if(inputValue === ""){ // todo handle the inputbox. if is empty show error state
+    const [postText, setPostText] = useState(undefined); // needed?
+    const [errorText, setErrorText] = useState(undefined);
+    const [errorState, setErrorState] = useState(undefined);
 
+    const handleChange = (event) => {
+        setPostText(event.target.value);
+        if (!event.target.value) {
+            setErrorState(true)
+            setErrorText("Name can't be blank")
+        } else {
+            setErrorState(false)
+            setErrorText("")
         }
     }
+    const handleClick = () => { // todo use condition or handle buttons separately
+        console.log("Hello From change avatar & banner")
+    }
+
     return (
         <>
             {show ?
                 <Modal open={show}
                        onClose={close}
                 >
-                    <Box sx={style}>
+                    <Box className="editUserBox">
                         <div>
                             <CardContent className="userSection">
                                 <div className="editProfileHeader">
                                     <div className="editProfileCloseBtn">
-                                        <CloseOutlined/>
+                                        <CloseOutlined onClick={close}/>
                                     </div>
                                     <div className="editProfileHeaderTitleAndBtn">
                                         <Typography className="editProfileTitle" variant="h5">
-                                        Edit Profile
-                                    </Typography>
-
+                                            Edit Profile
+                                        </Typography>
+                                    </div>
+                                </div>
+                                <div className="editBannerAvatarContainer">
+                                    <div>
+                                        <CardMedia className="banner"
+                                                   component="img"
+                                                   image={banner}
+                                                   alt="PP"
+                                        />
+                                        <div className="editAvatar">
+                                            <div className="profileCard">
+                                                <div className="profilePicBG">
+                                                    <CardMedia className="profileAvatar"
+                                                               component="img"
+                                                               image={pfp}
+                                                               alt="ProfilePic"
+                                                    />
+                                                </div>
+                                                <div>
+                                                    <Button
+                                                        className="userProfileAvatarButton"
+                                                        variant="outlined"
+                                                        onClick={handleClick}
+                                                    >Profile pic</Button>
+                                                    <Button
+                                                        className="userProfileAvatarButton"
+                                                        variant="outlined"
+                                                        onClick={handleClick}
+                                                    >Banner pic</Button>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="editProfile">
-                                        <TextField className="editProfileInputBoxes"
-                                            id="outlined-helperText"
-                                            label="Name"
-                                            defaultValue="This is my name" // show the actual value, hardcoded ftm
-                                            helperText="" // only show when input is empty
-                                        />
+                                    <TextField className="editProfileInputBoxes"
+                                               id="outlined-helperText"
+                                               label="Name"
+                                               error={errorState}
+                                               defaultValue="This is my name" // show the actual value, hardcoded ftm
+                                               helperText={errorText} // only show when input is empty
+                                               onChange={handleChange}
+                                    />
                                 </div>
                                 <div className="editProfile">
                                     <TextField className="editProfileInputBoxes"
@@ -59,20 +95,21 @@ const UserProfileModal = ({show, close}) => {
                                                helperText={inputValue} // only show when input is empty
                                     />
                                 </div>
-                                <div className="editProfile">
-                                    <Typography className="editProfileTitles">Location</Typography> {/* todo use api for location */}
+                                {/*<div className="editProfile">
+                                    <Typography
+                                        className="editProfileTitles">Location</Typography>  todo use api for location?
                                     <Typography className="styleASDASD" variant="subtitle2">
                                         Editable City, Country
                                     </Typography>
-                                </div>
-                                <div className="editProfile">
+                                </div>*/}
+                                {/*<div className="editProfile">
                                     <TextField className="editProfileInputBoxes"
                                                id="outlined-helperText"
                                                label=""
                                                defaultValue="Website" // show the actual value
                                                helperText="" // not needed
                                     />
-                                </div>
+                                </div>*/}
                                 <div className="editProfileFooter">
                                     <Button variant="outlined">Save</Button>
                                 </div>

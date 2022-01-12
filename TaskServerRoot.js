5bbac47f6a8a22ad5a2377f93acac676cba47470
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2022-01-12 18:00:09 +0100

    Crypto bug fixed

--- TaskServerRoot.js
+++ TaskServerRoot.js
@@ -60,7 +60,8 @@ SA.nodeModules = {
   discordTypes: require('discord-api-types/v9'),
   octokit: require('@octokit/rest'),
   graphql: require('@octokit/graphql'),
-  axios: require('axios')
+  axios: require('axios'),
+  crypto: require('crypto')
 }
 SA.version = require('./package.json').version
 /*

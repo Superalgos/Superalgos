1948cbd144777ef3f2235b90a54d358942b77b27
Claudio <claudiosuperalgos@gmail.com>
2022-01-14 20:15:56 -0300

    [Desktop] Removed const menuOptions Removed MenuItems Added Drawer component Updated Sidebar items Added a second Drawer. miniSidebar

--- DesktopReact/frontend/src/components/sidebar/Sidebar.js
+++ DesktopReact/frontend/src/components/sidebar/Sidebar.js
@@ -1,20 +1,13 @@
 import './Sidebar.css';
-import {Card, CardMedia, Divider, Stack, Button, Drawer, Box, IconButton} from "@mui/material";
+import {Avatar, Button, Card, CardMedia, Divider, Drawer, Stack} from "@mui/material";
 import SidebarItem from "../sidebarItem/SidebarItem";
 import Logo from "../logo/Logo";
-import {LoginOutlined} from '@mui/icons-material'
+import '../logo/Logo.css'
 import HomeIcon from '@mui/icons-material/Home';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import PersonIcon from '@mui/icons-material/Person';
 import pic from "../../images/superalgos.png";
-import React from "react";
 import SidebarUserProfile from "../sidebarUserProfile/SidebarUserProfile";
-import SidebarUserProfile from "../sidebarUserProfile/sidebarUserProfile";
-import MenuIcon from '@mui/icons-material/Menu';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import React, {useState} from "react";
 
 const Sidebar = props => {
@@ -35,30 +28,58 @@ const Sidebar = props => {
 
     return (
         <div className="sidebar">
+            <Drawer
+                variant="permanent"
+                PaperProps={{style: {position: "relative"}}}
+                sx={{ height: "100%",
+                    display: {xs: 'none', sm: 'none', md: 'none', lg: 'block'},
+                    '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},}}open>
             <Stack direction="column"
-                   justifyContent="flex-start"
-                   alignItems="center"
                    spacing={5}
                    sx={{minWidth: "max-content"}}>
                 <Logo/>
-                <Stack
-                    direction="column"
-                    justifyContent="space-between"
-                    alignItems="center"
+                <Stack className="sidebarContainer"
                     spacing={2}
                     divider={<Divider orientation="horizontal" flexItem/>}>
-                    {menuItems}
+                    <SidebarItem text={true} value="Home" Button={Button} Icon={HomeIcon}/>
+                    <SidebarItem text={true} value="Profile" Button={Button} Icon={PersonIcon}/>
+                    <SidebarItem text={true} value="Post" Button={Button} Icon={PostAddIcon}/>
                 </Stack>
             </Stack>
             <Card className="sidebarProfileCard" variant="outlined">
                 <CardMedia className="avatar"
                            component="img"
                            image={pic}
-                           alt="PP"
-
-                />
-                <SidebarUserProfile className="sidebarProfile"/>
+                           alt="PP"/>
+                <SidebarUserProfile/>
             </Card>
+            </Drawer>
+            <Drawer
+                variant="permanent"
+                PaperProps={{style: {position: "relative"}}}
+                sx={{ height: "100%",
+                    display: { xs: 'block', sm: 'block', md: 'block', lg: 'none' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth - 185},}}open>
+                <div className="sidebarMini">
+                    <Avatar className="icon" alt="SuperAlgos" src={pic}/>
+                    <Stack className="sidebarMiniContainer"
+                           spacing={2}
+                           divider={<Divider orientation="horizontal" flexItem/>}>
+                        <SidebarItem text={false} value="Home" Button={Button} Icon={HomeIcon}/>
+                        <SidebarItem text={false} value="Profile" Button={Button} Icon={PersonIcon}/>
+                        <SidebarItem text={false} value="Post" Button={Button} Icon={PostAddIcon}/>
+                    </Stack>
+                </div>
+                {/* todo broken with new merge, review */}
+                <Card className="sidebarProfileCard" variant="outlined" sx={{border: "0px"}}>
+                    <CardMedia className="avatar"
+                               sx={{ paddingLeft: "0px"}}
+                               component="img"
+                               image={pic}
+                               alt="PP"/>
+                    <SidebarUserProfile/>
+                </Card>
+            </Drawer>
         </div>
     );
 }

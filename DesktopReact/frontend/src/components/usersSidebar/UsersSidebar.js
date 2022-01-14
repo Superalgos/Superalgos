c13bfd8d695f32e402455aca0d788c60eab7b380
Claudio <claudiosuperalgos@gmail.com>
2022-01-14 20:22:04 -0300

    [Desktop] Added Drawer

--- DesktopReact/frontend/src/components/usersSidebar/UsersSidebar.js
+++ DesktopReact/frontend/src/components/usersSidebar/UsersSidebar.js
@@ -1,23 +1,27 @@
 import './UsersSidebar.css';
-import {Stack, Typography} from "@mui/material";
+import {Drawer, Stack, Typography} from "@mui/material";
 import SuggestedUsers from "../suggestedUsers/SuggestedUsers";
 
 
 const UsersSidebar = () => {
-
+    const drawerWidth = 240;
     const showMoreCallback = () => console.log("clicked show more");
     return (
-        <Stack 
-            direction="column"
-            justifyContent="flex-start"
-            alignItems="center"
-            spacing={2}
-            sx={{minWidth: "max-content"}}
-            >
-            {/*<SearchBox/>*/}
-            <Typography> Suggested Users</Typography>
-            <SuggestedUsers/>
-        </Stack>
+        <Drawer
+            variant="permanent"
+            PaperProps={{style: {position: "relative"}}}
+            sx={{display: {xs: 'none', sm: 'none', md: 'block'}}}>
+            <Stack
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="center"
+                spacing={2}
+                sx={{minWidth: "max-content"}}>
+                {/*<SearchBox/>*/}
+                <Typography> Suggested Users</Typography>
+                <SuggestedUsers/>
+            </Stack>
+        </Drawer>
     );
 }
 

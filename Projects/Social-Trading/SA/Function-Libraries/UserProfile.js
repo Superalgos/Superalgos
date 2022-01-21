01ef572f9663665e435085d9d5cd5b1f3e8ea7b6
Luis Fernando Molina <Luis-Fernando-Molina@users.noreply.github.com>
2022-01-21 12:53:18 +0100

    Adding nodes

--- Projects/Social-Trading/SA/Function-Libraries/UserProfile.js
+++ Projects/Social-Trading/SA/Function-Libraries/UserProfile.js
@@ -78,15 +78,17 @@ exports.newSocialTradingFunctionLibrariesUserProfile = function () {
                 resolve(JSON.stringify(response))
             }
 
-            const organization = 'Superalgos'
-            const repo = 'Governance-Project-Plugins'
-            const owner = profileMessage.storageProviderUsername
+            const SUPERALGOS_ORGANIZATION_NAME = 'Superalgos'
+            const GOVERNANCE_PLUGINS_REPO_NAME = 'Governance-Project-Plugins'
             const { Octokit } = SA.nodeModules.octokit
             const octokit = new Octokit({
                 auth: profileMessage.storageProviderToken,
                 userAgent: 'Superalgos ' + SA.version
             })
             let userProfile
+            let response = {
+                result: 'Ok'
+            }
 
             //await checkCreateFork()
             await checkCreateUserProfile()
@@ -100,20 +102,20 @@ exports.newSocialTradingFunctionLibrariesUserProfile = function () {
                 Step #1 : We will check if the storageProviderName has a fork of Superalgos already created. If not, we will create the fork.
                 */
                 await octokit.rest.repos.get({
-                    owner: owner,
-                    repo: repo
+                    owner: profileMessage.storageProviderUsername,
+                    repo: GOVERNANCE_PLUGINS_REPO_NAME
                 })
                     .then(repositoryFound)
                     .catch(repositoryNotFound)
 
                 async function repositoryFound(repository) {
-                    if (repository.data.parent.name === repo && repository.data.parent.owner.login === organization) {
+                    if (repository.data.parent.name === GOVERNANCE_PLUGINS_REPO_NAME && repository.data.parent.owner.login === SUPERALGOS_ORGANIZATION_NAME) {
                         /* We already have a fork, and we don't need to create one. */
                     } else {
                         /* We do not have a fork, and we need to create one. */
                         await octokit.rest.repos.createFork({
-                            owner: organization,
-                            repo: repo
+                            owner: SUPERALGOS_ORGANIZATION_NAME,
+                            repo: GOVERNANCE_PLUGINS_REPO_NAME
                         })
                             .then(succeedCreatingFork)
                             .catch(errorCreatingFork)
@@ -125,8 +127,8 @@ exports.newSocialTradingFunctionLibrariesUserProfile = function () {
                     if (err.status === 404) {
                         /* We do not have a fork, and we need to create one. */
                         await octokit.rest.repos.createFork({
-                            owner: organization,
-                            repo: repo
+                            owner: SUPERALGOS_ORGANIZATION_NAME,
+                            repo: GOVERNANCE_PLUGINS_REPO_NAME
                         })
                             .then(succeedCreatingFork)
                             .catch(errorCreatingFork)
@@ -159,13 +161,22 @@ exports.newSocialTradingFunctionLibrariesUserProfile = function () {
                 /*
                 Step #2 : We will check for the User Profile and read that file if exists. 
                 */
-                let userProfileUrl = 'https://raw.githubusercontent.com/' + organization + '/' + repo + '/develop/User-ProfilesXXX/' + owner + '.json'
+                let userProfileUrl =
+                    'https://raw.githubusercontent.com/' +
+                    SUPERALGOS_ORGANIZATION_NAME +
+                    '/' +
+                    GOVERNANCE_PLUGINS_REPO_NAME +
+                    '/develop/User-Profiles/' +
+                    profileMessage.storageProviderUsername +
+                    '.json'
+
                 await SA.projects.foundations.utilities.webAccess.fetchAPIDataFile(userProfileUrl)
                     .then(userProfileExists)
                     .catch(userProfileDoesNotExist)
 
                 function userProfileExists(fileContent) {
                     userProfile = JSON.parse(fileContent)
+                    response.message = "Existing User Profile Upgraded."
                 }
 
                 function userProfileDoesNotExist() {
@@ -174,26 +185,150 @@ exports.newSocialTradingFunctionLibrariesUserProfile = function () {
                     */
                     userProfile = {
                         type: 'User Profile',
-                        name: owner,
+                        name: profileMessage.storageProviderUsername,
                         project: 'Governance',
                         id: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId()
                     }
                     /*
                     Create a Wallet for the new User Profile and get the Private Key.
                     */
-
+                    const Web3 = SA.nodeModules.web3
+                    let web3 = new Web3()
+                    let account = web3.eth.accounts.create()
+                    let address = account.address
+                    let privateKey = account.privateKey
                     /*
                     Sign the User Profile with the Wallet Private Key.
                     */
+                    let signature = web3.eth.accounts.sign(profileMessage.storageProviderUsername, privateKey)
+                    let userProfileConfig = {
+                        signature: signature
+                    }
+                    userProfile.config = JSON.stringify(userProfileConfig)
+                    response.message = "New User Profile Created."
+                    response.address = address
+                    response.privateKey = privateKey
+                }
+            }
+
+            async function addUserApps() {
+                userProfile.userApps = {
+                    type: 'User Apps',
+                    name: 'New User Apps',
+                    project: 'User-Apps',
+                    id: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
+                    config: '{}'
+                }
+
+                switch (profileMessage.userAppType) {
+                    case "Social Trading Desktop App": {
+                        userProfile.userApps.desktopApps = {
+                            type: 'Desktop Apps',
+                            name: 'New Desktop Apps',
+                            project: 'User-Apps',
+                            id: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
+                            config: '{}',
+                            socialTradingDesktopApps: [
+                                {
+                                    type: 'Social Trading Desktop App',
+                                    name: 'New Social Trading Desktop App',
+                                    project: 'User-Apps',
+                                    id: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
+                                    config: '{}',
+                                }
+                            ]
+                        }
+                        break
+                    }
+                    case "Social Trading Mobile App": {
+                        userProfile.userApps.desktopApps = {
+                            type: 'Mobile Apps',
+                            name: 'New Mobile Apps',
+                            project: 'User-Apps',
+                            id: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
+                            config: '{}',
+                            socialTradingDesktopApps: [
+                                {
+                                    type: 'Social Trading Mobile App',
+                                    name: 'New Social Trading Mobile App',
+                                    project: 'User-Apps',
+                                    id: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
+                                    config: '{}',
+                                }
+                            ]
+                        }
+                        break
+                    }
                 }
             }
 
             async function addOpenStorage() {
 
-            }
+                const SOCIAL_TRADING_REPO_NAME = "My-Social-Trading-Data"
+                /*
+                Create this repository at Github 
+                */
+                await octokit.rest.repos.createForAuthenticatedUser({
+                    name: SOCIAL_TRADING_REPO_NAME,
+                    private: false
+                })
+                    .then(repositoryCreated)
+                    .catch(repositoryNotCreated)
 
-            async function addUserApps() {
+                function repositoryCreated() {
+                    addNodes()
+                }
 
+                function repositoryCreated(err) {
+                    if (err.status === 230) {
+                        /*
+                        Repo already existed, so no need to create it. 
+                        */
+                        addNodes()
+                    } else {
+                        let response = {
+                            result: 'Error',
+                            message: 'Error creating Repo at Github.',
+                            stack: err.stack
+                        }
+                        resolve(response)
+                    }
+                }
+
+                function addNodes() {
+                    /*
+                    Add the User Storage nodes to the User Profile
+                    */
+                    userProfile.userStorage = {
+                        type: 'User Storage',
+                        name: 'New User Storage',
+                        project: 'Open-Storage',
+                        id: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
+                        config: '{}',
+                        githubStorage: {
+                            type: 'Github Storage',
+                            name: 'New Github Storage',
+                            project: 'Open-Storage',
+                            id: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
+                            config: '{}',
+                            githubStorageContainers: [
+                                {
+                                    type: 'Github Storage Container',
+                                    name: 'New Github Storage Container',
+                                    project: 'Open-Storage',
+                                    id: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
+                                    config: JSON.stringify(
+                                        {
+                                            codeName: SOCIAL_TRADING_REPO_NAME,
+                                            githubUserName: profileMessage.storageProviderUsername,
+                                            repositoryName: SOCIAL_TRADING_REPO_NAME
+                                        }
+                                    )
+                                }
+                            ]
+                        }
+                    }
+                }
             }
 
             async function addSigningAccounts() {

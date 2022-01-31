ede29bb9383ba53e44a913d9c3553788cb412715
Claudio <claudiosuperalgos@gmail.com>
2022-01-31 17:12:24 -0300

    [Desktop] Added close button

--- DesktopReact/frontend/src/components/footerReplyModal/FooterReplyModal.js
+++ DesktopReact/frontend/src/components/footerReplyModal/FooterReplyModal.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Box, Modal} from "@mui/material";
+import {Box, IconButton, Modal} from "@mui/material";
 import "./FooterReplyModal.css"
 import ReplyBox from "../replyBox/ReplyBox";
 import PostBody from "../post/PostBody";
+import {CloseOutlined} from "@mui/icons-material";
 
 const FooterReplyModal = ({show, close, post}) => {
     return (
@@ -10,6 +11,11 @@ const FooterReplyModal = ({show, close, post}) => {
                onClose={close}
         >
             <Box className="replyModal">
+                <div className="repostHeader">
+                    <IconButton onClick={close}>
+                        <CloseOutlined/>
+                    </IconButton>
+                </div>
                 <div className="replyFeedContainer"> {/*feed of the post clicked*/}
                     <PostBody postData={post}/>
                 </div>

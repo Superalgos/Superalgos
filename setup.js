55fd3ec3ad866f1a380285b3fcf62141ac1fe5b9
Char1zardd <jsJ3vR@protonmail.com>
2022-02-18 19:51:20 -0500

    forgot to remove old imports from setup

--- setup.js
+++ setup.js
@@ -1,16 +1,5 @@
-const path = require("path")
-const fs = require("fs")
 const process = require("process")
-const { exec } = require("child_process")
 const systemCheck = require('./Launch-Scripts/systemCheck')
-const platform = require('os').platform()  // Possible values are: 'aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', and 'win32'.
-const https = require("https")
-const externalScriptsDir = path.join(process.cwd(), "Platform", "WebServer", "externalScripts")
-const externalScripts = [
-    "https://code.jquery.com/jquery-3.6.0.js",
-    "https://code.jquery.com/ui/1.13.0/jquery-ui.js"
-]
-const projectPluginMap = require('./Plugins/project-plugin-map.json')
 const createShortcut = require('./Launch-Scripts/createShortcut')
 const runSetup = require("./Launch-Scripts/runSetup")
 

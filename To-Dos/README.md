8efe889d3bbf1ca784839ca7af34264689f750a5
Char1zardd <jsJ3vR@protonmail.com>
2022-02-19 11:31:34 -0500

    rename test files, add todos, and rearrange runSetup for unit tests

---
+++ To-Dos/README.md
@@ -0,0 +1,14 @@
+# Next Up
+
+### Unit Testing
+
+* `setup.js`
+* `setupPlugins.js`
+
+# Done 
+
+### Unit Testing
+
+* `Environment.js`
+* `Launch-Scripts/runSetup.js`
+* `Launch-Scripts/createShortcut.js`
--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 const process = require("process")
 const systemCheck = require('./Launch-Scripts/systemCheck')
 const createShortcut = require('./Launch-Scripts/createShortcut')
-const runSetup = require("./Launch-Scripts/runSetup")
+const { runSetup } = require("./Launch-Scripts/runSetup")
 
 // Check system is set up correctly 
 systemCheck()
@@ -9,7 +9,7 @@ systemCheck()
 // Handle adding shortcuts
 if (process.argv.includes("noShortcuts")) {
     // Cancel running script if flag provided
-    console.log('');
+    console.log('')
     console.log('noShortcuts ................................................... Setting up without shortcuts.')
 
 } else {

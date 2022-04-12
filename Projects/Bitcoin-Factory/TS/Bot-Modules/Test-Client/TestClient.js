505867b4d0385f2ff20c53fd5c8632727ba586a2
Luis-Fernando-Molina <Luis-Fernando-Molina@users.noreply.github.com>
2022-04-12 11:28:07 +0200

    Update TestClient.js

--- Projects/Bitcoin-Factory/TS/Bot-Modules/Test-Client/TestClient.js
+++ Projects/Bitcoin-Factory/TS/Bot-Modules/Test-Client/TestClient.js
@@ -25,20 +25,51 @@
 
     async function start(callBackFunction) {
         try {
+            await getNextTestCase()
+                .then(onSuccess)
+                .catch(onError)
+            async function onSuccess(nextTestCase) {
+                if (nextTestCase !== undefined) {
+                    SA.nodeModules.fs.writeFileSync("./notebooks/parameters.csv", nextTestCase.files.parameters)
+                    SA.nodeModules.fs.writeFileSync("./notebooks/time-series.csv", nextTestCase.files.timeSeries)
 
-            let queryMessage = {
-                messageId: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
-                sender: 'Test-Client'
+                    await buildModel(nextTestCase)
+                        .then(onSuccess)
+                        .catch(onError)
+
+                    async function onSuccess(testResult) {
+                        if (testResult !== undefined) {
+                            testResult.id = nextTestCase.id
+                            await setTestCaseResults(testResult)
+                                .then(onSuccess)
+                                .catch(onError)
+                            async function onSuccess(response) {
+                                let bestPredictions = JSON.parse(response)
+                                console.log(' ')
+                                console.log('Best Crowd-Sourced Predictions:')
+                                console.table(bestPredictions)
+                                updateSuperalgos(bestPredictions)
+                            }
+                            async function onError(err) {
+                                console.log((new Date()).toISOString(), 'Failed to send a Report to the Test Server with the Test Case Results and get a Reward for that. Err:', err, 'Aborting the processing of this case and retrying the main loop in 10 seconds...')
+                            }
+                        }
+                    }
+
+                    async function onError(err) {
+                        console.log((new Date()).toISOString(), 'Failed to Build the Model for this Test Case. Err:', err, 'Aborting the processing of this case and retrying the main loop in 10 seconds...')
+                        await sleep(10000)
+                    }
+                } else {
+                    console.log((new Date()).toISOString(), 'Nothing to Test', 'Retrying in 10 seconds...')
+                }
+                await sleep(10000)
+            }
+            async function onError(err) {
+                console.log((new Date()).toISOString(), 'Failed to get a Test Case. Err:', err, 'Retrying in 10 seconds...')
+                await sleep(10000)
             }
 
-            let messageHeader = {
-                requestType: 'Query',
-                networkService: 'Machine Learning',
-                queryMessage: JSON.stringify(queryMessage)
-            }
-
-            let response = await TS.projects.foundations.globals.taskConstants.P2P_NETWORK.p2pNetworkClient.machineLearningNetworkServiceClient.sendMessage(messageHeader)
-            console.log(response)
             callBackFunction(TS.projects.foundations.globals.standardResponses.DEFAULT_OK_RESPONSE)
         }
         catch (err) {
@@ -47,5 +78,227 @@
                 "[ERROR] start -> err = " + err.stack)
             callBackFunction(TS.projects.foundations.globals.standardResponses.DEFAULT_FAIL_RESPONSE)
         }
+    }
+
+    async function run() {
+        while (true) {
+
+        }
+    }
+
+    function updateSuperalgos(bestPredictions) {
+
+        for (let i = 0; i < bestPredictions.length; i++) {
+            let prediction = bestPredictions[i]
+            prediction.parameters = {}
+        }
+
+        let params = {
+            method: 'updateForecastedCandles',
+            forcastedCandles: JSON.stringify(bestPredictions)
+        }
+
+        const axios = require("axios")
+        axios
+            .post('http://' + ENVIRONMENT.SUPERALGOS_HOST + ':' + ENVIRONMENT.SUPERALGOS_HTTP_PORT + '/Bitcoin-Factory', params)
+            .then(res => {
+                console.log((new Date()).toISOString(), 'Updating Superalgos...', 'Response from Superalgos Bitcoin Factory Server: ' + JSON.stringify(res.data))
+            })
+            .catch(error => {
+                console.log((new Date()).toISOString(), 'Updating Superalgos...', 'Could not update Superalgos. Had this error: ' + error)
+            })
+    }
+
+    async function getNextTestCase() {
+        return new Promise(promiseWork)
+
+        async function promiseWork(resolve, reject) {
+
+            let message = {
+                type: 'Get Next Test Case'
+            }
+
+            let queryMessage = {
+                messageId: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
+                sender: 'Test-Client',
+                message: message
+            }
+
+            let messageHeader = {
+                requestType: 'Query',
+                networkService: 'Machine Learning',
+                queryMessage: JSON.stringify(queryMessage)
+            }
+
+            await TS.projects.foundations.globals.taskConstants.P2P_NETWORK.p2pNetworkClient.machineLearningNetworkServiceClient.sendMessage(messageHeader)
+                .then(onSuccess)
+                .catch(onError)
+            async function onSuccess(response) {
+                if (response !== 'NO TEST CASES AVAILABLE AT THE MOMENT') {
+                    let nextTestCase = JSON.parse(response[2])
+                    nextTestCase.files.timeSeries = response[0]
+                    nextTestCase.files.parameters = response[1]
+                    resolve(nextTestCase)
+                } else {
+                    reject('No more test cases at the Test Server')
+                }
+            }
+            async function onError(err) {
+                reject(err)
+            }
+        }
+    }
+
+    async function setTestCaseResults(testResult) {
+        return new Promise(promiseWork)
+
+        async function promiseWork(resolve, reject) {
+            let message = {
+                type: 'Set Test Case Results',
+                payload: JSON.stringify(testResult)
+            }
+
+            let queryMessage = {
+                messageId: SA.projects.foundations.utilities.miscellaneousFunctions.genereteUniqueId(),
+                sender: 'Test-Client',
+                message: message
+            }
+
+            let messageHeader = {
+                requestType: 'Query',
+                networkService: 'Machine Learning',
+                queryMessage: JSON.stringify(queryMessage)
+            }   
+
+            await TS.projects.foundations.globals.taskConstants.P2P_NETWORK.p2pNetworkClient.machineLearningNetworkServiceClient.sendMessage(messageHeader)
+                .then(onSuccess)
+                .catch(onError)
+            async function onSuccess(response) {
+                resolve(response)
+            }
+            async function onError(err) {
+                reject(err)
+            }
+        }
+    }
+
+    async function buildModel(nextTestCase) {
+        console.log('')
+        console.log('-------------------------------------------------------- Test Case # ' + nextTestCase.id + ' / ' + nextTestCase.totalCases + ' --------------------------------------------------------')
+        console.log('')
+        console.log((new Date()).toISOString(), 'Starting processing this Case')
+        console.log('')
+        console.log('Parameters Received for this Test:')
+        console.table(nextTestCase.parameters)
+        console.log('')
+
+        let processExecutionResult
+        let startingTimestamp = (new Date()).valueOf()
+        return new Promise(promiseWork)
+
+        async function promiseWork(resolve, reject) {
+
+            const { spawn } = require('child_process');
+            const ls = spawn('docker', ['exec', 'Bitcoin-Factory-ML', 'python', '/tf/notebooks/Bitcoin_Factory_LSTM.py']);
+            let dataReceived = ''
+            ls.stdout.on('data', (data) => {
+                data = data.toString()
+                /*
+                Removing Carriedge Return from string.
+                */
+                if (ENVIRONMENT.LOG_TRAINING_OUTPUT === true) {
+                    console.log(data)
+                }
+                for (let i = 0; i < 1000; i++) {
+                    data = data.replace(/\n/, "")
+                }
+                dataReceived = dataReceived + data.toString()
+            });
+
+            ls.stderr.on('data', (data) => {
+                onError(data)
+            });
+
+            ls.on('close', (code) => {
+                console.log(`Docker Python Script exited with code ${code}`);
+                if (code === 0) {
+                    onFinished(dataReceived)
+                } else {
+                    console.log((new Date()).toISOString(), '[ERROR] Unexpected error trying to execute a Python script inside the Docker container. ')
+                    console.log((new Date()).toISOString(), '[ERROR] Check at a console if you can run this command: ')
+                    console.log((new Date()).toISOString(), '[ERROR] docker exec -it Bitcoin-Factory-ML python /tf/notebooks/Bitcoin_Factory_LSTM.py')
+                    console.log((new Date()).toISOString(), '[ERROR] Once you can sucessfully run it at the console you might want to try to run this App again. ')
+                    reject('Unexpected Error.')
+                }
+            });
+
+            function onError(err) {
+                err = err.toString()
+                //reject('Error Building Model.')
+            }
+
+            function onFinished(dataReceived) {
+                try {
+
+                    processExecutionResult = JSON.parse(dataReceived)
+                    processExecutionResult.predictions = fixJSON(processExecutionResult.predictions)
+                    processExecutionResult.predictions = JSON.parse(processExecutionResult.predictions)
+
+                    console.log('Prediction RMSE Error: ' + processExecutionResult.errorRMSE)
+                    console.log('Predictions [candle.max, candle.min, candle.close]: ' + processExecutionResult.predictions)
+
+                    let endingTimestamp = (new Date()).valueOf()
+                    processExecutionResult.enlapsedTime = (endingTimestamp - startingTimestamp) / 1000
+                    console.log('Enlapsed Time (HH:MM:SS): ' + (new Date(processExecutionResult.enlapsedTime * 1000).toISOString().substr(14, 5)) + ' ')
+
+                } catch (err) {
+
+                    if (processExecutionResult !== undefined && processExecutionResult.predictions !== undefined) {
+                        console.log('processExecutionResult.predictions:' + processExecutionResult.predictions)
+                    }
+
+                    console.log(err.stack)
+                    console.error(err)
+                }
+
+                resolve(processExecutionResult)
+            }
+        }
+    }
+
+    function sleep(ms) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms)
+        })
+    }
+
+    function fixJSON(text) {
+        /*
+        Removing Carriedge Return from string.
+        */
+        for (let i = 0; i < 10; i++) {
+            text = text.replace(" [", "[")
+            text = text.replace(" ]", "]")
+            text = text.replace("  ]", "]")
+            text = text.replace("   ]", "]")
+            text = text.replace("    ]", "]")
+            text = text.replace("     ]", "]")
+            text = text.replace("      ]", "]")
+            text = text.replace("] ", "]")
+        }
+        for (let i = 0; i < 100; i++) {
+            text = text.replace("  ", ",")
+        }
+        for (let i = 0; i < 100; i++) {
+            text = text.replace(" ", ",")
+        }
+        for (let i = 0; i < 10; i++) {
+            text = text.replace(",,", ",")
+            text = text.replace(",]", "]")
+            text = text.replace("[,", "[")
+            text = text.replace(".,", ",")
+            text = text.replace(".]", "]")
+        }
+        return text
     }
 }

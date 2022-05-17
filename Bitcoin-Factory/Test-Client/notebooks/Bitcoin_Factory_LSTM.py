fa0de6ca86939426ef7333419b8bba59bd8a200e
Gary Ward <88884874+DJ05GaZ@users.noreply.github.com>
2022-05-17 13:06:36 +0100

    Added continue after error and warning of bad lines when reading CSV file.

--- Bitcoin-Factory/Test-Client/notebooks/Bitcoin_Factory_LSTM.py
+++ Bitcoin-Factory/Test-Client/notebooks/Bitcoin_Factory_LSTM.py
@@ -77,7 +77,9 @@ parameters_dataset = read_csv(
     '/tf/notebooks/parameters.csv', 
     header=0, 
     sep=' ', 
-    skipinitialspace=True
+    skipinitialspace=True,
+    error_bad_lines=False,
+    warn_bad_lines=True
 )
 
 parameters_dataset
@@ -118,7 +120,9 @@ timeseries_dataset = read_csv(
     header=0, 
     index_col=0,    #The first colum is a timestamp that will be used to index all the data.
     sep=' ', 
-    skipinitialspace=True
+    skipinitialspace=True,
+    error_bad_lines=False,
+    warn_bad_lines=True
 )
 
 timeseries_dataset

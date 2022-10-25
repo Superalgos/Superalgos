be9a940b86d4b82bd72c00b1b38fbd9a9b08d23e
Alex White <alex_p_white@yahoo.co.uk>
2022-10-25 06:51:07 +0100

    added index file for initial build testing

--- Projects/Education/PL/Utilities/ExportFunctions.js
+++ Projects/Education/PL/Utilities/ExportFunctions.js
@@ -7,12 +7,24 @@ exports.newEducationUtilitiesExportFunctions = function () {
     return thisObject
 
     function process(body) {
-        const payload = JSON.parse(body)
-        const filePath = global.env.PATH_TO_EXPORT_DOCS + '/' + `${payload.project}/${payload.category}/`
-        SA.projects.foundations.utilities.filesAndDirectories.createNewDir(filePath)
-        
-        const fileName = `${payload.type.replace(' ', '-')}.html`
+        try {
+            const payload = JSON.parse(body)
+            const filePath = global.env.PATH_TO_EXPORT_DOCS + '/' + `${payload.project}/${payload.category}/`
+            SA.projects.foundations.utilities.filesAndDirectories.createNewDir(filePath)
+            
+            const fileName = `${payload.type.replace(' ', '-')}.html`
+    
+            const content = readIndexTemplateFile().replace('%docs-content-div%', payload.content)
+    
+            SA.nodeModules.fs.writeFileSync(filePath + fileName, content)
+        }
+        catch(error) {
+            console.error(error)
+        }
+    }
 
-        SA.nodeModules.fs.writeFileSync(filePath + fileName, payload.content)
+    function readIndexTemplateFile() {
+        const contents = SA.nodeModules.fs.readFileSync(global.env.PATH_TO_EXPORT_DOCS + '/index.html')
+        return contents.toString('utf8')
     }
 }

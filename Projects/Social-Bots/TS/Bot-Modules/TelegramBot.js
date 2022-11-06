1a8715b987f4e06e2b485ad0df28b04a7482bd98
Alex White <alex_p_white@yahoo.co.uk>
2022-11-06 14:48:33 +0000

    added catch statement to telegram initialize launch request

--- Projects/Social-Bots/TS/Bot-Modules/TelegramBot.js
+++ Projects/Social-Bots/TS/Bot-Modules/TelegramBot.js
@@ -44,8 +44,8 @@ exports.newSocialBotsBotModulesTelegramBot = function newSocialBotsBotModulesTel
                     thisObject.telegramAPI = thisObject.telegramBot.telegram
 
                     const message = "Telegram bot is starting. For assistance type /help."
-                    thisObject.telegramAPI.sendMessage(thisObject.chatId, message).catch(err => logError('[WARN] initialize -> Telegram API error -> err = ' + err.stack))
-                })
+                    thisObject.telegramAPI.sendMessage(thisObject.chatId, message).catch(err => logError('[WARN] initialize.sendMessage -> Telegram API error -> err = ' + err.stack))
+                }).catch(err => logError('[WARN] initialize.launch -> Telegram API error -> err = ' + err.stack))
 
 
         } catch (err) {

fa2ad0b37df446aa2188c94e481f1c1efab8ddec
Alex White <alex_p_white@yahoo.co.uk>
2022-11-07 10:14:24 +0000

    cleaned out unused code

--- Exports/Docs/Scripts/DocumentationExporter.js
+++ Exports/Docs/Scripts/DocumentationExporter.js
@@ -84,7 +84,6 @@ exports.documentationExporter = function() {
 
         getSchemaDocument()
         buildHtmlPage()
-        // enableCollapsibleContent()
 
         function getSchemaDocument() {
             switch (thisObject.currentDocumentBeingRendered.category) {
@@ -2059,31 +2058,6 @@ exports.documentationExporter = function() {
             return '<a href="' + link + '"> ' + content + ' </a>'
         }
     }
-
-    // function enableCollapsibleContent() {
-    //     let collapsibleElementsArray = document.getElementsByClassName("docs-collapsible-element")
-
-    //     for (let i = 0; i < collapsibleElementsArray.length; i++) {
-    //         collapsibleElementsArray[i].addEventListener("click", function () {
-    //             this.classList.toggle("docs-collapsible-active")
-    //             let content = this.nextElementSibling
-    //             if (content.style.display === "block") {
-    //                 content.style.display = "none";
-    //             } else {
-    //                 content.style.display = "block";
-    //             }
-    //         })
-    //     }
-    // }
-
-    // function disableCollapsibleContent() {
-    //     let collapsibleElementsArray = document.getElementsByClassName("docs-collapsible-element")
-
-    //     for (let i = 0; i < collapsibleElementsArray.length; i++) {
-    //         collapsibleElementsArray[i].removeEventListener("click", function () {
-    //         })
-    //     }
-    // }
 
     function addFooter() {
 

70aa0bc1ce8a61a257af143fa206d761268da13e
harrellbm <brendenharrell1@gmail.com>
2022-11-28 21:40:46 -0600

    Basic setup of Vite and UI

---
+++ SocialTrade/VueUI/src/main.js
@@ -0,0 +1,32 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from './views/HomeView.vue'
+import DevelopersView from './views/DevelopersView.vue'
+import BitcoinFactoryView from './views/BitcoinFactoryView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/Developers',
+    name: 'Developers',
+    component: DevelopersView
+  },
+  {
+    path: '/BitcoinFactory',
+    name: 'BitcoinFactory',
+    component: BitcoinFactoryView
+  }
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+createApp(App).use(router).mount('#app')

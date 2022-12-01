5b94ba8f1fa901134f184e3663632ad83a8368e4
Nuno Amorim <na.amorim@gmail.com>
2022-12-01 23:14:52 +0000

    Fix some typos

--- Platform/Client/webSocketsInterface.js
+++ Platform/Client/webSocketsInterface.js
@@ -34,9 +34,9 @@
                     console.log('New Websocket Connection.')
                 }
 
-                socket.on('message', onMenssage)
+                socket.on('message', onMessage)
 
-                function onMenssage(message) {
+                function onMessage(message) {
                     // Here is where all messages will be received through the websocket
        
                     try {

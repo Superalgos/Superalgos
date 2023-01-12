ff1ee041eb192353381b668275d1c2104b8da1c9
Alex White <alex_p_white@yahoo.co.uk>
2023-01-12 21:32:50 +0000

    added more features to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if which apt > /dev/null
+then
+    operator="apt"
+elif which yum > /dev/null
+then
+    operator="yum"
+fi
+
 while getopts u:t: flag
 do
     case "${flag}" in
@@ -17,28 +25,52 @@ then
     echo "Please supply a Github token with repository write access"
 fi
 
+installed() {
+    echo ""
+    echo "superalgos | info | installed $1"
+    echo ""
+}
 
 installGit() {
     echo ""
     echo "superalgos | info | installing git"
     echo ""
-    add-apt-repository ppa:git-core/ppa
-    apt update
-    apt install git -y
-    echo ""
-    echo "superalgos | info | installed git"
-    echo ""
+    if [ "$operator" = "apt" ]
+    then
+        add-apt-repository ppa:git-core/ppa
+        apt update
+        apt install git -y
+        installed "git"
+    elif [ "$operator" = "yum" ]
+    then
+        yum install git -y
+        installed "git"
+    else
+        echo "superalgos | error | unable to install git on this linux distribution please contact support to see if it can be added to the install script"
+        echo "===================="
+        exit 1
+    fi
 }
 
 installNode() {
     echo ""
     echo "superalgos | info | installing nodejs"
     echo ""
-    curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
-    sudo apt-get install -y nodejs
-    echo ""
-    echo "superalgos | info | installed nodejs"
-    echo ""
+    if [ "$operator" = "apt" ]
+    then
+        curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
+        sudo apt-get install -y nodejs
+        installed "nodejs"
+    elif [ "$operator" = "yum" ]
+    then
+        curl -sL https://rpm.nodesource.com/setup_19.x | sudo bash -
+        sudo yum install nodejs
+        installed "nodejs"
+    else
+        echo "superalgos | error | unable to install nodejs on this linux distribution please contact support to see if it can be added to the install script"
+        echo "===================="
+        exit 1
+    fi
 }
 
 checkGit() {

414dcf94a541d632d6171218a780fff9a2668c57
Alex White <alex_p_white@yahoo.co.uk>
2023-01-25 20:48:16 +0000

    install script updates

--- install.sh
+++ install.sh
@@ -1,29 +1,148 @@
 #!/bin/sh
 
-install() {
-    # install pm2 globally
-    echo "superalgos | info | installing pm2 globally for process management\n"
-    npm install -g pm2
+if which apt > /dev/null
+then
+    operator="apt"
+elif which yum > /dev/null
+then
+    operator="yum"
+fi
 
-    # install all the local modules
-    echo "superalgos | info | installing local node dependencies\n"
-    npm install
+while getopts u:t: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        t) token=${OPTARG};;
+    esac
+done
 
-    # apply security fixes to local modules
-    echo "superalgos | info | applying security patches for local dependencies\n"
-    npm audit fix --force
+if [ -z "$username" ]
+then
+    echo "Please supply your Github username"
+fi
+if [ -z "$token" ]
+then
+    echo "Please supply a Github token with repository write access"
+fi
 
-    # install the superalgos command line app
-    echo "superalgos | info | installing 'superalgos' cli\n"
-    npm install -g .
-
-    echo "superalgos | info | running node setup script to run prepare the app\n"
-    node setup.js
+installed() {
+    echo ""
+    echo "superalgos | info | installed $1"
+    echo ""
 }
 
-if which node > /dev/null
-then
+installGit() {
+    echo ""
+    echo "superalgos | info | installing git"
+    echo ""
+    if [ "$operator" = "apt" ]
+    then
+        add-apt-repository ppa:git-core/ppa
+        apt update
+        apt install git -y
+        installed "git"
+    elif [ "$operator" = "yum" ]
+    then
+        yum install git -y
+        installed "git"
+    else
+        echo "superalgos | error | unable to install git on this linux distribution please contact support to see if it can be added to the install script"
+        echo "===================="
+        exit 1
+    fi
+}
+
+installNode() {
+    echo ""
+    echo "superalgos | info | installing nodejs"
+    echo ""
+    if [ "$operator" = "apt" ]
+    then
+        curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
+        sudo apt-get install -y nodejs
+        installed "nodejs"
+    elif [ "$operator" = "yum" ]
+    then
+        curl -sL https://rpm.nodesource.com/setup_19.x | sudo bash -
+        sudo yum install nodejs
+        installed "nodejs"
+    else
+        echo "superalgos | error | unable to install nodejs on this linux distribution please contact support to see if it can be added to the install script"
+        echo "===================="
+        exit 1
+    fi
+}
+
+checkGit() {
+    if which git > /dev/null
+    then
+        echo ""
+        echo "superalgos | info | git already installed"
+        echo ""
+    else
+        installGit
+    fi
+}
+
+checkNode() {
+    if which node > /dev/null
+    then
+        echo ""
+        echo "superalgos | info | nodejs already installed"
+        echo ""
+    else
+        installNode
+    fi
+}
+
+cloneSuperalgos() {
+    echo ""
+    echo "superalgos | info | downloading application code"
+    echo ""
+    git clone "https://github.com/$username/Superalgos.git"
+}
+
+install() {
+    # install pm2 globally
+    echo ""
+    echo "superalgos | info | installing pm2 globally for process management"
+    echo ""
+    npm install -g pm2
+
+    echo ""
+    echo "superalgos | info | running node setup script to run prepare the app"
+    echo ""
+    node setup.js
+
+    echo ""
+    echo "superalgos | info | running node setup script to run prepare the app"
+    echo ""
+    node setupPlugins.js $username $token
+
+    # install the superalgos command line app
+    echo ""
+    echo "superalgos | info | installing 'superalgos' cli"
+    echo ""
+    npm install -g . --omit=optional
+
+    # apply security fixes to local modules
+    echo ""
+    echo "superalgos | info | applying security patches for local dependencies"
+    echo ""
+    npm audit fix --force --omit=optional
+}
+
+main() {
+    checkGit
+    checkNode
+    cloneSuperalgos
+    cd Superalgos
     install
-else
-    echo "node is not installed, please install nodejs to run Superalgos\n"
+}
+
+if [ -z "$username" ] || [ -z "$token" ]
+then
+    exit 1
 fi
+
+main

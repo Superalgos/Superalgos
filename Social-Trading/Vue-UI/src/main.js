aa66474c5d0cc785dc07e8f014f4f73ded90f239
End-debug <65282771+end-debug@users.noreply.github.com>
2023-03-14 15:53:15 +0200

    Update main.js

--- Social-Trading/Vue-UI/src/main.js
+++ Social-Trading/Vue-UI/src/main.js
@@ -3,6 +3,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')

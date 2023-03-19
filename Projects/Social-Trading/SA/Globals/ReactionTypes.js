29356264ad8710d416c2ed69b567fe5b64cb7fe8
theblockchainarborist <theblockchainarborist@gmail.com>
2023-03-18 21:19:47 -0400

    Reactions should be setup on Back-End

--- Projects/Social-Trading/SA/Globals/ReactionTypes.js
+++ Projects/Social-Trading/SA/Globals/ReactionTypes.js
@@ -4,11 +4,14 @@ exports.newSocialTradingGlobalsReactionTypes = function () {
         /* Reaction Types */
         REACTION_LIKE: 0,
         REACTION_LOVE: 1,
-        REACTION_HAHA: 2,
-        REACTION_WOW: 3,
-        REACTION_SAD: 4,
-        REACTION_ANGRY: 5,
-        REACTION_CARE: 6
+        ADD_REACTION_SMILE_SMALL_EYES: 2,
+        ADD_REACTION_BIG_CHEESING: 3,
+        ADD_REACTION_TEARS_OF_JOY: 4,
+        ADD_REACTION_HEART_EYES: 5,
+        ADD_REACTION_PINOCCHIO: 6,
+        ADD_REACTION_CELEBRATION: 7,
+        ADD_REACTION_SURPRISED_GHOST: 8,
+        ADD_REACTION_FRUSTRATED_MONKEY: 9
     }
 
     return thisObject

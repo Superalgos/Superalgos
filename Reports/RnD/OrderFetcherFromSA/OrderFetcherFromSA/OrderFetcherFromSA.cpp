469c3d80ec2cfdb01700f3bedf4bce939dce287e
BigGremlin <gregcolley@bigpond.com>
2023-05-13 11:09:41 +0100

    New Order Fetcher (Direct from SA)

---
+++ Reports/RnD/OrderFetcherFromSA/OrderFetcherFromSA/OrderFetcherFromSA.cpp
@@ -0,0 +1,12 @@
+﻿// OrderFetcherFromSA.cpp : Defines the entry point for the application.
+//
+
+#include "OrderFetcherFromSA.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Reports/RnD/OrderFetcherFromSA/OrderFetcherFromSA/OrderFetcherFromSA.h
@@ -0,0 +1,8 @@
+﻿// OrderFetcherFromSA.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.

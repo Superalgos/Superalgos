a7520566de1be0602f3b9b46aabb5982e70f7936
BigGremlin <gregcolley@bigpond.com>
2023-05-26 09:40:57 +0100

    Remove files for early development on direct analysis from SA logs (Migrated to OrderFetcherFromSA)

--- Reports/RnD/AnalysisDirectFromSA/AnalysisDirectFromSA/AnalysisDirectFromSA.h
+++
@@ -1,8 +0,0 @@
-﻿// AnalysisDirectFromSA.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.

26f58b13b87ed5593f396151f5c7f1dcb1e3eb2c
Todd Stephens <todstephens80@gmail.com>
2023-05-27 13:38:18 -0400

    Update desktop-app-3.js
       python -m pip install git+https://github.com/mamba-org/conda_oci_mirror@main
              echo ${{ env.GHA_PAT }} | oras login https://ghcr.io -u ${{ env.GITHUB_USER }} --password-stdin
              conda-oci mirror --channel ${{ github.event.inputs.channel }} -s ${{ matrix.subdir }} --packages ${{ github.event.inputs.package }}  --user ${{ env.GITHUB_USER }}

--- desktop-app-3.js
+++ desktop-app-3.js
@@ -6,3 +6,6 @@ defined at the Environment.js file.
 let APP_ROOT = require('./DesktopRoot.js')
 let APP_ROOT_MODULE = APP_ROOT.newDesktopRoot()
 APP_ROOT_MODULE.run({DESKTOP_APP_SIGNING_ACCOUNT: 'Social-Trading-Desktop-App-3'})
+   python -m pip install git+https://github.com/mamba-org/conda_oci_mirror@main
+          echo ${{ env.GHA_PAT }} | oras login https://ghcr.io -u ${{ env.GITHUB_USER }} --password-stdin
+          conda-oci mirror --channel ${{ github.event.inputs.channel }} -s ${{ matrix.subdir }} --packages ${{ github.event.inputs.package }}  --user ${{ env.GITHUB_USER }}

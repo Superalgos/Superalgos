999fc0c247b33380d7bf072b4438a1e8952af378
Alex White <alex_p_white@yahoo.co.uk>
2024-02-23 18:36:54 +0000

    updated network help

--- network.js
+++ network.js
@@ -11,6 +11,8 @@ USAGE:
     ${chalk.red('node network')} ${chalk.italic.red('[options]')}
         
 OPTIONS:
+    - ${chalk.italic.red('--network-signing-account')} ${chalk.bold.red('text')}
+            A specific P2P network signing account to start using. This overrides the default "P2P-Network-Node-1"
     - ${chalk.italic.red('--include-local-networks')} ${chalk.bold.red('true/false')}
             Do you want to include all local networks nodes?
     - ${chalk.italic.red('--only-me')} ${chalk.bold.red('true/false')}
@@ -19,6 +21,13 @@ OPTIONS:
             Repeat for each username you want to connect to
 
 EXAMPLES:
+
+    ############################################################
+    # To specify a network node on your profile called
+    # ${chalk.italic.red('Local-Network-Node')}
+    ############################################################
+    node network --network-signing-account Local-Network-Node
+
     ############################################################
     # To limit the network connections to only my network nodes, 
     # in order for this to work you will need at least 2 network 
@@ -70,29 +79,42 @@ function buildFilters(args) {
         }
         filters.users = args.users
     }
-    console.log(JSON.stringify(filters))
     return filters
 }
 
 yargs(hideBin(process.argv))
     .version(require('./package.json').version)
     .alias('h', 'help')
-    .command('$0', 'the default command', () => {}, (args) => {
-        let APP_ROOT = require('./NetworkRoot.js')
-        let APP_ROOT_MODULE = APP_ROOT.newNetworkRoot()
-        APP_ROOT_MODULE.run(undefined, buildFilters(args))
+    .command('$0', helpMessage, () => {}, (args) => {
+        if(args.help) {
+            console.log(helpMessage)
+        }
+        else {
+            let APP_ROOT = require('./NetworkRoot.js')
+            let APP_ROOT_MODULE = APP_ROOT.newNetworkRoot()
+            let debugSettings = args.networkSigningAccount === undefined ? undefined : {P2P_NETWORK_NODE_SIGNING_ACCOUNT: args.networkSigningAccount}
+            APP_ROOT_MODULE.run(debugSettings, buildFilters(args))
+        }
     })
     .option('include-local-networks', {
         boolean: true,
         demandOption: false,
+        describe: `To exclude all local networks from the connection pool set the ${chalk.italic.red('--include-local-networks')} to ${chalk.bold.red('false')}. This is useful as  many users will use localhost or a 192 style address when  testing their own network setup and this will create multiple connections self referencing connections.`
     })
     .option('only-me', {
         boolean: true,
         demandOption: false,
+        describe: `To limit the network connections to only my network nodes, in order for this to work you will need at least 2 network nodes setup`
     })
     .option('users', {
         array: true,
         demandOption: false,
+        describe: `To create a network pool with a friend or some specfic users then add as many ${chalk.italic.red('--users <USER>')} options as you need. This will limit the network connection pool to only these user networks.`
     })
-    .help(helpMessage)
+    .option('network-signing-account', {
+        string: true,
+        demandOption: false,
+        describe: `To specify a network node on your profile called ${chalk.bold.red('Local-Network-Node')}`
+    })
+    .help()
     .parse()
